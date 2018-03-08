6674d5673ad03c702c6e7e30438dc6bf5c283405
ning <ningyuan.sg@gmail.com>
2018-03-08 12:51:16 +0800

    Update version ('0.0.1a' -> '0.1.0')

--- slang.js
+++ slang.js
@@ -6,7 +6,7 @@
 const repl = require('./lib/repl.js')
 const program = require('commander');
 program
-  .version('slang 0.0.1a', '-v, --version')
+  .version('slang 0.1.0', '-v, --version')
   .parse(process.argv);
 
 repl.startRepl();
