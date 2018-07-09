2e86aa2d663f7b35cadff49e81bc81d247fb5f0e
ning <ningyuan.sg@gmail.com>
2018-07-09 13:51:54 +0800

    Add jest (tests fail)

---
+++ toolchain/mocks/context.ts
@@ -0,0 +1,39 @@
+import { parse } from 'acorn'
+import * as es from 'estree'
+
+import { createContext } from '../index'
+import { Closure, Context, Frame } from '../types'
+import { TypeError } from '../utils/rttc'
+
+export function mockContext(chapter = 1): Context {
+  return createContext(chapter)
+}
+
+export function mockRuntimeContext(): Context {
+  const context = createContext()
+  context.runtime = {
+    isRunning: true,
+    frames: [],
+    nodes: [
+      {
+        type: 'Literal',
+        loc: {
+          start: { line: 1, column: 0 },
+          end: { line: 1, column: 1 }
+        },
+        value: 0,
+        raw: '0',
+        range: [0, 1]
+      }
+    ]
+  }
+  return context
+}
+
+export function mockClosure(): Closure {
+  return new Closure({} as es.FunctionExpression, {} as Frame, {} as Context)
+}
+
+export function mockTypeError(): TypeError {
+  return new TypeError(parse(''), '', '', '')
+}
