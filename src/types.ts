a713aaff5b3e99328587dc4cbef6f592d8116052
remo5000 <vigneshshankar@gmail.com>
2018-07-11 10:53:09 +0800

    Change return type of alert to void

--- src/types.ts
+++ src/types.ts
@@ -12,7 +12,7 @@ import { closureToJS } from './interop'
 export interface CustomBuiltIns {
   display: (value: Value, externalContext: any) => void,
   prompt: (value: value, externalContext: any) => string | null,
-  alert: (value: value, externalContext: any) => null,
+  alert: (value: value, externalContext: any) => void,
   /* Used for list visualisation. See #12 */
   visualiseList: (list: any, externalContext: any) => null
 }
