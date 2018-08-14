460e0bf94ae137e6324c2a126ee0faa8039b4455
remo5000 <vigneshshankar@gmail.com>
2018-08-14 20:43:18 +0800

    Use && for failing tsc

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,6 @@
-# Compile all .ts files
-tsc
-# Copy the only .js file, parser.js, to dist.
+# Compile all .ts files, then
+# copy the only .js file, parser.js, to dist.
 # This is required because it is not possible to both inlcude .js
 # files (allowJs = true) and export type declarations (declarations = true) 
 # in tsconfig. See https://github.com/Microsoft/TypeScript/issues/7546
-cp src/stdlib/parser.js dist/stdlib
+tsc && cp src/stdlib/parser.js dist/stdlib
