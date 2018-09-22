c10e0f7bf493971b4fdf75b5c7a9aab6a58de3c6
ning <ningyuan.sg@gmail.com>
2018-09-23 01:22:54 +0800

    Add jest tests

---
+++ src/__tests__/interpreter-errors.ts
@@ -0,0 +1,17 @@
+import { mockContext } from "../mocks/context";
+import { runInContext, parseError } from "../index";
+import { Finished } from "../types";
+
+test("Undefined variable error is thrown", () => {
+  const code = `
+    im_undefined;
+  `
+  const context = mockContext()
+  const promise = runInContext(code, context, { scheduler: "preemptive" })
+  return promise.then(obj => {
+    expect(obj).toMatchSnapshot()
+    expect(obj.status).toBe("error")
+    expect(context.errors).toMatchSnapshot()
+    expect(parseError(context.errors)).toBe("Line 2: Name im_undefined not declared")
+  })
+})
