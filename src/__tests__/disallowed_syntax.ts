41dfecf92bcd84a1d1740dc1c164da282becc14d
Yuchuan <yyc478@gmail.com>
2018-10-19 20:28:58 +0800

    Add failing test for disallowed function expression

--- src/__tests__/disallowed_syntax.ts
+++ src/__tests__/disallowed_syntax.ts
@@ -57,3 +57,32 @@ test("Cannot use update expressions", () => {
   });
 });
 
+test("Cannot use js function expressions", () => {
+  const code = `
+  map(function(x) {
+    return x + 1;
+  }, list(1));
+  `;
+  const context = mockContext(3);
+  const promise = runInContext(code, context, { scheduler: "preemptive" });
+  return promise.then(obj => {
+    expect(obj.status).toBe("error");
+    const errors = parseError(context.errors);
+    expect(errors).toMatchSnapshot();
+  });
+});
+
+test("DEFINITELY CANNOT use named function declarations as expressions", () => {
+  const code = `
+  map(function a(x) {
+    return x + 1;
+  }, list(1));
+  `;
+  const context = mockContext(3);
+  const promise = runInContext(code, context, { scheduler: "preemptive" });
+  return promise.then(obj => {
+    expect(obj.status).toBe("error");
+    const errors = parseError(context.errors);
+    expect(errors).toMatchSnapshot();
+  });
+});
