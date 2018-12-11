6cd9eee8b7e4d7443425e55f79f291abcb55f2df
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2018-12-12 06:24:12 +0800

    Add explicit test for declaration without assignment

--- src/__tests__/disallowed_syntax.ts
+++ src/__tests__/disallowed_syntax.ts
@@ -174,6 +174,19 @@ test('Cannot use destructuring declarations', () => {
   })
 })
 
+test('no declaration without assignment', () => {
+  const code = `
+  let x;
+  `
+  const context = mockContext(100)
+  const promise = runInContext(code, context, { scheduler: 'preemptive' })
+  return promise.then(obj => {
+    expect(obj.status).toBe('error')
+    const errors = parseError(context.errors)
+    expect(errors).toMatchSnapshot()
+  })
+})
+
 test('Cannot use update statements', () => {
   const code = `
   let x = 3;
