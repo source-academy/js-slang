d6a1ad2ef08b90cb08062a0d361778e3ce7b8bb2
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2018-12-12 15:52:37 +0800

    Implement basic repl (#158)
    * Implement basic repl
    
    * Add REPL usage to readme

---
+++ src/repl/repl.ts
@@ -0,0 +1,26 @@
+import { parseError, runInContext, createContext } from '../index'
+import { Result } from '../types'
+import repl = require('repl')
+
+function startRepl(chapter = 1) {
+  // use defaults for everything
+  let context = createContext(chapter)
+  repl.start({
+    eval: (cmd, _context, _filename, callback) => {
+      runInContext(cmd, context, { scheduler: 'preemptive' }).then((obj: Result) => {
+        if (obj.status === 'finished') {
+          callback(null, obj.value)
+        } else {
+          callback(new Error(parseError(context.errors)), undefined)
+        }
+      })
+    }
+  })
+}
+
+function main() {
+  let chapter = process.argv.length > 2 ? parseInt(process.argv[2]) : 1
+  startRepl(chapter)
+}
+
+main()
