39170895e9cee6747faf82121cee2d07d8f2cec3
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2018-12-15 23:32:06 +0800

    Remove now unused stdlib/metacircular-interpreter

--- src/stdlib/metacircular-interpreter/envsetup.bat
+++
@@ -1,5 +0,0 @@
-npm install --global gcc-rest jison markup-js node-zip nomnom 
-npm link gcc-rest jison markup-js node-zip nomnom
-patch -i jison.patch node_modules\jison\lib\jison.js
-
-node generate.js --environment
