9b5fbbfec434ad2f73e037827cea98b3c9403a3c
LeowWB <41581512+LeowWB@users.noreply.github.com>
2019-02-25 17:22:44 +0800

    added accessor method to check verbose state of interpreter

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,10 @@ export function parseError(errors: SourceError[], verbose: boolean = verboseErro
   return errorMessagesArr.join('\n')
 }
 
+export function verboseEnabled() {
+  return verboseErrors
+}
+
 export function runInContext(
   code: string,
   context: Context,
