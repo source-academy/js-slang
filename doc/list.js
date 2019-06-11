c7f8de5825d3cfd2fe803c49378412ea12db8d88
Martin Henz <henz@comp.nus.edu.sg>
2019-06-11 10:27:04 +0800

    single source of truth for libraries

---
+++ doc/list.js
@@ -0,0 +1,200 @@
+// \texttt{list.js START} \begin{lstlisting}
+
+// is_list recurses down the list and checks that it ends with the empty list null
+
+function is_list(xs) {
+    return is_null(xs) || (is_pair(xs) && is_list(tail(xs)));
+}
+
+// equal computes the structural equality 
+// over its arguments
+
+function equal(x, y){
+    return (is_pair(x) && is_pair(y)) 
+        ? (equal(head(x), head(y)) &&
+           equal(tail(x), tail(y)))
+        : x === y;
+}
+
+// returns the length of a given argument list
+// assumes that the argument is a list
+
+function length(xs) {
+    return is_null(xs) 
+	? 0
+        : 1 + length(tail(xs));
+}
+
+// map applies first arg f, assumed to be a unary function,
+// to the elements of the second argument, assumed to be a list.
+// f is applied element-by-element: 
+// map(f, list(1, 2)) results in list(f(1), f(2))
+
+function map(f, xs) {
+    return is_null(xs)
+        ? null
+        : pair(f(head(xs)), map(f, tail(xs)));
+}
+
+// build_list takes a non-negative integer n as first argument,
+// and a function fun as second argument.
+// build_list returns a list of n elements, that results from 
+// applying fun to the numbers from 0 to n-1.
+
+function build_list(n, fun){
+    function build(i, fun, already_built) {
+        return i < 0
+            ? already_built
+            : build(i - 1, fun, pair(fun(i),
+                already_built));
+    }
+    return build(n - 1, fun, null);
+}
+
+// for_each applies first arg fun, assumed to be a unary function,
+// to the elements of the second argument, assumed to be a list.
+// fun is applied element-by-element:
+// for_each(fun, list(1, 2)) results in the calls fun(1) and fun(2).
+// for_each returns true.
+
+function for_each(fun, xs) {
+    if (is_null(xs)) {
+        return true;
+    } else {
+        fun(head(xs));
+        return for_each(fun, tail(xs));
+    }
+}
+
+// to_string uses JavaScript's + to turn its argument into a string
+
+function to_string(x) {
+    return x + "";
+}
+
+// list_to_string returns a string that represents the argument list.
+// It applies itself recursively on the elements of the given list.
+// When it encounters a non-list, it applies to_string to it.
+
+function list_to_string(xs) {
+    return is_null(xs)
+        ? "null"
+        : is_pair(xs)
+            ? "[" + list_to_string(head(xs)) + ","+
+                    list_to_string(tail(xs))      + "]"
+            : to_string(xs);
+}
+
+// reverse reverses the argument, assumed to be a list
+
+function reverse(xs) {
+    function rev(original, reversed) {
+        return is_null(original)
+            ? reversed
+            : rev(tail(original), 
+                  pair(head(original), reversed));
+    }
+    return rev(xs, null);
+}
+
+// append first argument, assumed to be a list, to the second argument.
+// In the result null at the end of the first argument list
+// is replaced by the second argument, regardless what the second
+// argument consists of.
+
+function append(xs, ys) {
+    return is_null(xs)
+        ? ys
+        : pair(head(xs),
+            append(tail(xs), ys));
+} 
+
+// member looks for a given first-argument element in the 
+// second argument, assumed to be a list. It returns the first 
+// postfix sublist that starts with the given element. It returns null if the 
+// element does not occur in the list
+
+function member(v, xs){
+    return is_null(xs)
+        ? null
+        : (v === head(xs))
+            ? xs
+            : member(v, tail(xs));
+}
+
+// removes the first occurrence of a given first-argument element
+// in second-argument, assmed to be a list. Returns the original 
+// list if there is no occurrence.
+
+function remove(v, xs){
+    return is_null(xs)
+        ? null
+        : v === head(xs)
+            ? tail(xs)
+            : pair(head(xs), 
+                   remove(v, tail(xs)));
+}
+
+// Similar to remove, but removes all instances of v
+// instead of just the first
+
+function remove_all(v, xs) {
+    return is_null(xs)
+        ? null
+        : v === head(xs)
+            ? remove_all(v, tail(xs))
+            : pair(head(xs), 
+                     remove_all(v, tail(xs)));
+}
+
+// filter returns the sublist of elements of the second argument
+// (assumed to be a list), for which the given predicate function
+// returns true.
+
+function filter(pred, xs){
+    return is_null(xs)
+        ? xs
+        : pred(head(xs))
+            ? pair(head(xs),
+                   filter(pred, tail(xs)))
+            : filter(pred, tail(xs));
+}
+
+// enumerates numbers starting from start, assumed to be a number,
+// using a step size of 1, until the number exceeds end, assumed
+// to be a number
+
+function enum_list(start, end) {
+    return start > end
+        ? null
+        : pair(start,
+               enum_list(start + 1, end));
+}
+
+// Returns the item in xs (assumed to be a list) at index n,
+// assumed to be a non-negative integer.
+// Note: the first item is at position 0
+
+function list_ref(xs, n) {
+    return n === 0
+        ? head(xs)
+        : list_ref(tail(xs), n - 1);
+}
+
+// accumulate applies an operation op (assumed to be a binary function)
+// to elements of sequence (assumed to be a list) in a right-to-left order.
+// first apply op to the last element and initial, resulting in r1, then to
+// the  second-last element and r1, resulting in r2, etc, and finally
+// to the first element and r_n-1, where n is the length of the
+// list.
+// accumulate(op, zero, list(1, 2, 3)) results in
+// op(1, op(2, op(3, zero)))
+
+function accumulate(f, initial, xs) {
+    return is_null(xs)
+        ? initial
+        : f(head(xs),
+             accumulate(f, initial, tail(xs)));
+}
+
+// \end{lstlisting} // \texttt{list.js END}
---
+++ doc/stream.js
@@ -0,0 +1,212 @@
+// \texttt{stream.js START} \begin{lstlisting}
+
+// Supporting streams in the Scheme style, following
+// "stream discipline"
+
+// is_stream recurses down the stream and checks that it ends with the
+// empty list null
+
+function is_stream(xs) {
+    return is_null(xs) || (is_pair(xs) && is_list(stream_tail(xs)));
+}
+
+// A stream is either null or a pair whose tail is
+// a nullary function that returns a stream.
+
+function list_to_stream(xs) {
+    return is_null(xs)
+        ? null
+        : pair(head(xs), 
+               () => list_to_stream(tail(xs))); 
+}
+
+// stream_to_list transforms a given stream to a list
+// Lazy? No: stream_to_list needs to force the whole stream
+function stream_to_list(xs) {
+    return is_null(xs)
+        ? null
+        : pair(head(xs), stream_to_list(stream_tail(xs)));
+}
+
+// stream_length returns the length of a given argument stream
+// throws an exception if the argument is not a stream
+// Lazy? No: The function needs to explore the whole stream
+function stream_length(xs) {
+    return is_null(xs)
+        ? 0
+        : 1 + stream_length(stream_tail(xs));
+}
+
+// stream_map applies first arg f to the elements of the second
+// argument, assumed to be a stream.
+// f is applied element-by-element:
+// stream_map(f,list_to_stream(list(1,2)) results in
+// the same as list_to_stream(list(f(1),f(2)))
+// stream_map throws an exception if the second argument is not a
+// stream, and if the second argument is a non-empty stream and the
+// first argument is not a function.
+// Lazy? Yes: The argument stream is only explored as forced by
+//            the result stream.
+function stream_map(f, s) {
+    return is_null(s)
+        ? null
+        : pair(f(head(s)), 
+                 () => stream_map(f, stream_tail(s)));
+}
+
+// build_stream takes a non-negative integer n as first argument,
+// and a function fun as second argument.
+// build_list returns a stream of n elements, that results from
+// applying fun to the numbers from 0 to n-1.
+// Lazy? Yes: The result stream forces the applications of fun
+//            for the next element
+function build_stream(n, fun) {
+    function build(i) {
+        return i >= n
+            ? null
+            : pair(fun(i), 
+                   () => build(i + 1)); 
+    }
+    return build(0);
+}
+
+// stream_for_each applies first arg fun to the elements of the stream
+// passed as second argument. fun is applied element-by-element:
+// for_each(fun,list_to_stream(list(1, 2,null))) results in the calls fun(1)
+// and fun(2).
+// stream_for_each returns true.
+// stream_for_each throws an exception if the second argument is not a 
+// stream, and if the second argument is a non-empty stream and the
+// first argument is not a function.
+// Lazy? No: stream_for_each forces the exploration of the entire stream
+function stream_for_each(fun, xs) {
+    if (is_null(xs)) {
+        return true;
+    } else {
+        fun(head(xs));
+        return stream_for_each(fun, stream_tail(xs));
+    }
+}
+
+// stream_reverse reverses the argument stream
+// stream_reverse throws an exception if the argument is not a stream.
+// Lazy? No: stream_reverse forces the exploration of the entire stream
+function stream_reverse(xs) {
+    function rev(original, reversed) {
+        return is_null(original)
+            ? reversed
+            : rev(stream_tail(original), 
+                  pair(head(original), () => reversed));
+    }
+    return rev(xs,null);
+}
+
+// stream_append appends first argument stream and second argument stream.
+// In the result, null at the end of the first argument stream
+// is replaced by the second argument stream
+// stream_append throws an exception if the first argument is not a
+// stream.
+// Lazy? Yes: the result stream forces the actual append operation
+function stream_append(xs, ys) {
+    return is_null(xs)
+        ? ys
+        : pair(head(xs),
+               () => stream_append(stream_tail(xs), ys)); 
+}
+
+// stream_member looks for a given first-argument element in a given
+// second argument stream. It returns the first postfix substream
+// that starts with the given element. It returns null if the
+// element does not occur in the stream
+// Lazy? Sort-of: stream_member forces the stream only until the element is found.
+function stream_member(x, s) {
+    return is_null(s)
+        ? null
+        : head(s) === x
+            ? s
+            : stream_member(x, stream_tail(s));
+}
+
+// stream_remove removes the first occurrence of a given first-argument element
+// in a given second-argument list. Returns the original list
+// if there is no occurrence.
+// Lazy? Yes: the result stream forces the construction of each next element
+function stream_remove(v, xs) {
+    return is_null(xs)
+        ? null
+        : v === head(xs)
+            ? stream_tail(xs)
+            :  pair(head(xs), 
+                    () => stream_remove(v, stream_tail(xs))); 
+}
+
+// stream_remove_all removes all instances of v instead of just the first.
+// Lazy? Yes: the result stream forces the construction of each next element
+function stream_remove_all(v, xs) {
+    return is_null(xs)
+        ? null
+        : v === head(xs)
+            ? stream_remove_all(v, stream_tail(xs))
+            : pair(head(xs), () => stream_remove_all(v, stream_tail(xs))); 
+}
+
+// filter returns the substream of elements of given stream s
+// for which the given predicate function p returns true.
+// Lazy? Yes: The result stream forces the construction of
+//            each next element. Of course, the construction
+//            of the next element needs to go down the stream
+//            until an element is found for which p holds.
+function stream_filter(p, s) {
+    return is_null(s)
+        ? null
+        : p(head(s))
+            ? pair(head(s), 
+                   () => stream_filter(p, stream_tail(s)))
+            : stream_filter(p, stream_tail(s));
+}
+
+// enumerates numbers starting from start,
+// using a step size of 1, until the number
+// exceeds end.
+// Lazy? Yes: The result stream forces the construction of
+//            each next element
+function enum_stream(start, end) {
+    return start > end
+        ? null
+        : pair(start, 
+               () => enum_stream(start + 1, end)); 
+}
+
+// integers_from constructs an infinite stream of integers
+// starting at a given number n
+// Lazy? Yes: The result stream forces the construction of
+//            each next element
+function integers_from(n) {
+    return pair(n,
+                () => integers_from(n + 1));
+}
+
+// eval_stream constructs the list of the first n elements
+// of a given stream s
+// Lazy? Sort-of: eval_stream only forces the computation of
+//                the first n elements, and leaves the rest of
+//                the stream untouched.
+function eval_stream(s, n) {
+    return n === 0
+        ? null
+        : pair(head(s),
+               eval_stream(stream_tail(s),
+                           n - 1));
+}
+
+// Returns the item in stream s at index n (the first item is at position 0)
+// Lazy? Sort-of: stream_ref only forces the computation of
+//                the first n elements, and leaves the rest of
+//                the stream untouched.
+function stream_ref(s, n) {
+    return n === 0
+        ? head(s)
+        : stream_ref(stream_tail(s), n - 1);
+}
+
+// \end{lstlisting} // \texttt{stream.js END}
