ff10625e04d423a5c35a44db9fe94433925d9c69
Martin Henz <henz@comp.nus.edu.sg>
2019-08-04 17:36:16 +0800

    missing js files as sources for jsdoc

---
+++ doc/array.js
@@ -0,0 +1,19 @@
+/**
+ * returns
+ * the current length of array <CODE>x</CODE>, which is 1 plus the
+ * highest index that has been used so far in an array assignment on 
+ * <CODE>x</CODE>. Here literal array expressions are counted too: The
+ * array <CODE>[10, 20, 30]</CODE> has a length of 3.
+ * @param {array} x - given array
+ * @returns {number} current length of array
+ */
+function array_length(x) {}
+
+/**
+ * returns <CODE>true</CODE> if <CODE>x</CODE> 
+ * is an array, and <CODE>false</CODE> if it is not.
+ * @param {value} x - given value
+ * @returns {boolean} whether <CODE>x</CODE> is an array
+ */
+function is_array(x) {}
+
