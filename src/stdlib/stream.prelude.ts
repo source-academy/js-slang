71e57c9ecd1e1009f85157acb74ffe31ddf8fe19
Shuming <43642522+geshuming@users.noreply.github.com>
2019-09-21 13:48:41 +0800

    Fix is_stream function

--- src/stdlib/stream.prelude.ts
+++ src/stdlib/stream.prelude.ts
@@ -7,7 +7,7 @@ export const streamPrelude = `
 // empty list null
 
 function is_stream(xs) {
-  return is_null(xs) || (is_pair(xs) && is_list(stream_tail(xs)));
+  return is_null(xs) || (is_pair(xs) && is_stream(stream_tail(xs)));
 }
 
 // A stream is either null or a pair whose tail is
