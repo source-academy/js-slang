93a747d2aed598cf4b42c5a60beae44996b672b3
anubh-v <e0176401@u.nus.edu>
2020-03-23 14:52:00 +0800

    Provide utility functions for test code

--- src/__tests__/non-det-interpreter.ts
+++ src/__tests__/non-det-interpreter.ts
@@ -1,5 +1,5 @@
-/* tslint:disable:max-line-length */
-import { runInContext, resume, IOptions } from '../index'
+  /* tslint:disable:max-line-length */
+import { runInContext, resume, IOptions, Result } from '../index'
 import { mockContext } from '../mocks/context'
 import { SuspendedNonDet, Finished } from '../types'
 
@@ -16,35 +16,32 @@ test('Empty code returns undefined', async () => {
 test('Deterministic calculation', async () => {
   await testDeterministicCode('1 + 4 - 10 * 5;', -45)
 })
-
-async function testDeterministicCode(code: string, expectedValue: any) {
-  const context = makeNonDetContext()
-  let result = await runInContext(code, context, nonDetTestOptions)
-  expect((result as SuspendedNonDet).value).toBe(expectedValue)
-  expect(result.status).toBe('suspended-non-det')
-  // deterministic code only has a single value
-  result = await resume(result)
-  expect(result.status).toBe('finished')
-  expect((result as Finished).value).toBe(undefined)
-}
 // ---------------------------------- Non deterministic code tests -------------------------------
 
 test('Test simple amb application', async () => {
-  const context = makeNonDetContext()
-  let result = await runInContext('amb(1, 4 + 5, 3 - 10);', context, nonDetTestOptions)
-  expect(result.status).toBe('suspended-non-det')
-  expect((result as SuspendedNonDet).value).toBe(1)
-  result = await resume(result)
-  expect(result.status).toBe('suspended-non-det')
-  expect((result as SuspendedNonDet).value).toBe(9)
-  result = await resume(result)
-  expect(result.status).toBe('suspended-non-det')
-  expect((result as SuspendedNonDet).value).toBe(-7)
+  await testNonDeterministicCode('amb(1, 4 + 5, 3 - 10);', [1, 9, -7])
+})
 
-  result = await resume(result)
+async function testDeterministicCode(code: string, expectedValue: any) {
+  /* a deterministic program is equivalent to a non deterministic program
+     that returns a single value */
+  testNonDeterministicCode(code, [expectedValue])
+}
+
+async function testNonDeterministicCode(code: string, expectedValues: any[]) {
+  const context = makeNonDetContext()
+  let result: Result = await runInContext(code, context, nonDetTestOptions)
+  const numOfRuns = expectedValues.length;
+  for (let i = 0; i < numOfRuns; i++) {
+    expect((result as SuspendedNonDet).value).toBe(expectedValues[i])
+    expect(result.status).toBe('suspended-non-det')
+    result = await resume(result)
+  }
+
+  // all non deterministic programs have a final result whose value is undefined
   expect(result.status).toBe('finished')
   expect((result as Finished).value).toBe(undefined)
-})
+}
 
 function makeNonDetContext() {
   const context = mockContext(4)
