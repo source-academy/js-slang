c4f5af276a108602c3cdfacbae4f37f3d4ba623a
anubh-v <e0176401@u.nus.edu>
2020-03-23 14:54:26 +0800

    Shift code

--- src/__tests__/non-det-interpreter.ts
+++ src/__tests__/non-det-interpreter.ts
@@ -1,12 +1,7 @@
-  /* tslint:disable:max-line-length */
+/* tslint:disable:max-line-length */
 import { runInContext, resume, IOptions, Result } from '../index'
 import { mockContext } from '../mocks/context'
 import { SuspendedNonDet, Finished } from '../types'
-
-const nonDetTestOptions = {
-  scheduler: 'preemptive',
-  executionMethod: 'non-det-interpreter'
-} as Partial<IOptions>
 
 // ---------------------------------- Deterministic code tests --------------------------------
 test('Empty code returns undefined', async () => {
@@ -22,6 +17,13 @@ test('Test simple amb application', async () => {
   await testNonDeterministicCode('amb(1, 4 + 5, 3 - 10);', [1, 9, -7])
 })
 
+// ---------------------------------- Helper functions  -------------------------------------------
+
+const nonDetTestOptions = {
+  scheduler: 'preemptive',
+  executionMethod: 'non-det-interpreter'
+} as Partial<IOptions>
+
 async function testDeterministicCode(code: string, expectedValue: any) {
   /* a deterministic program is equivalent to a non deterministic program
      that returns a single value */
@@ -31,7 +33,7 @@ async function testDeterministicCode(code: string, expectedValue: any) {
 async function testNonDeterministicCode(code: string, expectedValues: any[]) {
   const context = makeNonDetContext()
   let result: Result = await runInContext(code, context, nonDetTestOptions)
-  const numOfRuns = expectedValues.length;
+  const numOfRuns = expectedValues.length
   for (let i = 0; i < numOfRuns; i++) {
     expect((result as SuspendedNonDet).value).toBe(expectedValues[i])
     expect(result.status).toBe('suspended-non-det')
