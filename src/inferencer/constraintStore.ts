c4816522e065f0b6ca5808892bcbcad014d23879
Richardson <rw0694@gmail.com>
2020-04-11 16:31:27 +0800

    fixed rule 1

--- src/inferencer/constraintStore.ts
+++ src/inferencer/constraintStore.ts
@@ -10,62 +10,64 @@ export function updateTypeConstraints(newConstraintLhs: Type, newConstraintRhs: 
 }
 
 function solveConstraint(constraintLhs: Type, constraintRhs: Type) {
-  // check if both key and value are not base types (Rule 1)
-  if (!is_base_type(constraintLhs) && !is_base_type(constraintRhs)) {
-    // check if key is not a type variable and value is a type variable (Rule 2)
-    if (is_base_type(constraintLhs) && is_type_variable(constraintRhs)) {
-      constraintStore.set(constraintRhs, constraintLhs)
-    }
-    // Rule 3
-    else if (
-      is_type_variable(constraintLhs) &&
-      constraintStore.get(constraintRhs) !== undefined &&
-      is_type_variable(constraintStore.get(constraintRhs)) &&
-      constraintStore.get(constraintRhs) === constraintLhs
-    ) {
-      // do nothing
-    }
-    // Rule 4
-    // else if (is_type_variable(constraintLhs) && is_function_type(constraintStore.get(constraintRhs))
-    //   && ) {
-    //   // error
-    //   console.log('Error! Rule 4')
-    // }
-    // Rule 5
-    else if (
-      (constraintLhs as Variable).isAddable &&
-      constraintStore.get(constraintRhs) !== undefined &&
-      !is_type_variable(constraintStore.get(constraintRhs)) &&
-      ((constraintRhs as Primitive).name !== 'number' ||
-        (constraintRhs as Primitive).name !== 'string')
-    ) {
-      // console.log('Error! Rule 5')
-    }
-    // Rule 6
-    else if (is_type_variable(constraintLhs) && constraintStore.get(constraintLhs) !== undefined) {
-      constraintStore.set(constraintRhs, constraintStore.get(constraintLhs))
-    }
-    // Rule 7
-    else if (
-      is_type_variable(constraintLhs) &&
-      constraintStore.get(constraintLhs) === undefined &&
-      constraintStore.get(constraintRhs) !== undefined
-    ) {
-      constraintStore.set(constraintLhs, constraintStore.get(constraintRhs))
-    } else {
-      constraintStore.set(constraintLhs, constraintRhs)
-    }
+  // check if both key and value are base types (Rule 1)
+  if (is_base_type(constraintLhs) && is_base_type(constraintRhs)) {
+    // do nothing
   }
+  // check if key is not a type variable and value is a type variable (Rule 2)
+  else if (is_base_type(constraintLhs) && is_type_variable(constraintRhs)) {
+    constraintStore.set(constraintRhs, constraintLhs)
+  }
+  // Rule 3
+  else if (
+    is_type_variable(constraintLhs) &&
+    constraintStore.get(constraintRhs) !== undefined &&
+    is_type_variable(constraintStore.get(constraintRhs)) &&
+    constraintStore.get(constraintRhs) === constraintLhs
+  ) {
+    // do nothing
+  }
+  // Rule 4
+  // else if (is_type_variable(constraintLhs) && is_function_type(constraintStore.get(constraintRhs))
+  //   && ) {
+  //   // error
+  //   console.log('Error! Rule 4')
+  // }
+  // Rule 5
+  else if (
+    (constraintLhs as Variable).isAddable &&
+    constraintStore.get(constraintRhs) !== undefined &&
+    !is_type_variable(constraintStore.get(constraintRhs)) &&
+    ((constraintRhs as Primitive).name !== 'number' ||
+      (constraintRhs as Primitive).name !== 'string')
+  ) {
+    // console.log('Error! Rule 5')
+  }
+  // Rule 6
+  else if (is_type_variable(constraintLhs) && constraintStore.get(constraintLhs) !== undefined) {
+    constraintStore.set(constraintRhs, constraintStore.get(constraintLhs))
+  }
+  // Rule 7
+  else if (
+    is_type_variable(constraintLhs) &&
+    constraintStore.get(constraintLhs) === undefined &&
+    constraintStore.get(constraintRhs) !== undefined
+  ) {
+    constraintStore.set(constraintLhs, constraintStore.get(constraintRhs))
+  } else {
+    constraintStore.set(constraintLhs, constraintRhs)
+  }
+
   // console.log('After solving')
   // constraintStore.forEach((value, key) => console.log(key, value))
 }
 
 function is_base_type(type: Type) {
-    return type.kind === 'primitive'
+  return type.kind === 'primitive'
 }
 
 function is_type_variable(type: Type) {
-    return type.kind === 'variable'
+  return type.kind === 'variable'
 }
 
 // function is_function_type(type: Type) {
