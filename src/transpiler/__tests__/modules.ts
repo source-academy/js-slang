278e644a312c79c4c0ac177b91a379e3f30e58ff
Hou-Rui <13244639785@163.com>
2020-04-13 19:07:30 +0800

    add tests for single import declaration

--- src/transpiler/__tests__/modules.ts
+++ src/transpiler/__tests__/modules.ts
@@ -1,7 +1,18 @@
-import { transformImportDeclarations } from '../transpiler'
+import { transformSingleImportDeclaration, transformImportDeclarations } from '../transpiler'
 import { stripIndent } from '../../utils/formatters'
 import { parse } from '../../parser/parser'
 import { mockContext } from '../../mocks/context'
+import { ImportDeclaration, Identifier } from 'estree'
+
+test('Transform single import decalration', () => {
+  const code = `import { foo, bar } from "test/one_module";`
+  const context = mockContext(4)
+  const program = parse(code, context)!
+  const result = transformSingleImportDeclaration(123, program.body[0] as ImportDeclaration)
+  const names = result.map(decl => (decl.declarations[0].id as Identifier).name)
+  expect(names[0]).toStrictEqual('foo')
+  expect(names[1]).toStrictEqual('bar')
+})
 
 test('Transform import decalrations variable decalarations', () => {
   const code = stripIndent`
