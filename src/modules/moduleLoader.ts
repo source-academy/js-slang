f11a2aa78c8c1db7e9578c7c4b411a0adea2a04a
Hou-Rui <13244639785@163.com>
2020-04-13 14:56:02 +0800

    add error handling

--- src/modules/moduleLoader.ts
+++ src/modules/moduleLoader.ts
@@ -2,13 +2,16 @@ import { XMLHttpRequest as NodeXMLHttpRequest } from 'xmlhttprequest-ts'
 const HttpRequest = typeof window === 'undefined' ? NodeXMLHttpRequest : XMLHttpRequest
 
 // TODO: Change this URL to actual Backend URL
-const BACKEND_STATIC_URL = 'http://0.0.0.0:4000/static'
+const BACKEND_STATIC_URL = 'http://ec2-54-169-81-133.ap-southeast-1.compute.amazonaws.com/static'
 
 export function loadIIFEModuleText(path: string) {
   const scriptPath = `${BACKEND_STATIC_URL}/${path}.js`
   const req = new HttpRequest()
   req.open('GET', scriptPath, false)
   req.send(null)
+  if (req.status != 200 && req.status != 304) {
+    throw new Error(`module ${path} not found.`)
+  }
   return req.responseText
 }
 
