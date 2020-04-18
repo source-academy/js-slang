182bdc2d09bb6aecdef5a169517e751c67372bff
Zijian Zhou <zephyroszhou@gmail.com>
2020-04-19 07:07:01 +0800

    Add support for type inference display (#552)
    * revamp source mode
    
    * revamp source mode
    
    * simplify code
    
    * add support for type inference display
    
    * prettify
    
    * delete unecessary codes
    
    * improve inference message and add comments to code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { simple } from 'acorn-walk/dist/walk'
+import { simple, findNodeAt } from 'acorn-walk/dist/walk'
 import { DebuggerStatement, Literal, Program, SourceLocation } from 'estree'
 import { RawSourceMap, SourceMapConsumer } from 'source-map'
 import { JSSLANG_PROPERTIES, UNKNOWN_LOCATION } from './constants'
@@ -27,7 +27,8 @@ import {
   Result,
   Scheduler,
   SourceError,
-  Variant
+  Variant,
+  TypeAnnotatedNode
 } from './types'
 import { nonDetEvaluate } from './interpreter/interpreter-non-det'
 import { locationDummyNode } from './utils/astCreator'
@@ -37,6 +38,8 @@ import { runWithProgram } from './vm/svml-machine'
 export { SourceDocumentation } from './editors/ace/docTooltip'
 import { getProgramNames, getKeywords } from './name-extractor'
 import * as es from 'estree'
+import { typeCheck } from './typeChecker/typeChecker'
+import { typeToString } from './utils/stringify'
 
 export interface IOptions {
   scheduler: 'preemptive' | 'async'
@@ -236,6 +239,92 @@ export async function getNames(
   const [progNames, displaySuggestions] = getProgramNames(program, comments, cursorLoc)
   const keywords = getKeywords(program, cursorLoc, context)
   return [progNames.concat(keywords), displaySuggestions]
+}
+
+function typedParse(code: any, context: Context) {
+  const program: Program | undefined = parse(code, context)
+  if (program === undefined) {
+    return null
+  }
+  return validateAndAnnotate(program, context)
+}
+
+export function getTypeInformation(
+  code: string,
+  context: Context,
+  loc: { line: number; column: number },
+  name: string
+): string {
+  const lineNumber = loc.line
+
+  // parse the program into typed nodes and parse error
+  const program = typedParse(code, context)
+  if (program === null) {
+    return ''
+  }
+  const [typedProgram, error] = typeCheck(program)
+  const parsedError = parseError(error)
+
+  // initialize the ans string
+  let ans = ''
+  if (parsedError) {
+    ans += parsedError + '\n'
+  }
+  if (!typedProgram) {
+    return ans
+  }
+
+  // callback function for findNodeAt function
+  function findByLocationPredicate(type: string, node: TypeAnnotatedNode<es.Node>) {
+    if (!node.inferredType) {
+      return false
+    }
+    const location = node.loc
+    const nodeType = node.type
+    if (nodeType && location) {
+      let id = ''
+      if (node.type === 'Identifier') {
+        id = node.name
+      } else if (node.type === 'FunctionDeclaration') {
+        id = node.id?.name!
+      } else if (node.type === 'VariableDeclaration') {
+        id = (node.declarations[0].id as es.Identifier).name
+      }
+      return id === name && location.start.line <= loc.line && location.end.line >= loc.line
+    }
+    return false
+  }
+
+  // report both as the type inference
+  return (
+    ans +
+    typedProgram.body
+      .map((node: TypeAnnotatedNode<es.Node>) => {
+        const res = findNodeAt(typedProgram, undefined, undefined, findByLocationPredicate)
+        if (res === undefined) {
+          return undefined
+        } else {
+          return res.node
+        }
+      })
+      .filter((node: TypeAnnotatedNode<es.Node>) => {
+        return node !== undefined
+      })
+      .map((node: TypeAnnotatedNode<es.Node>) => {
+        let id = ''
+        if (node.type === 'Identifier') {
+          id = node.name
+        } else if (node.type === 'FunctionDeclaration') {
+          id = node.id?.name!
+        } else if (node.type === 'VariableDeclaration') {
+          id = (node.declarations[0].id as es.Identifier).name
+        }
+        const type = typeToString(node.inferredType!)
+        return `At Line ${lineNumber} => ${id}: ${type}`
+      })
+      .slice(0, 1)
+      .join('\n')
+  )
 }
 
 export async function runInContext(
