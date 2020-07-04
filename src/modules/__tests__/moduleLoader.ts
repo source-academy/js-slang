3f693e282b1d9eac12e9882208a01695dc174ddf
angelsl <angelsl@in04.sg>
2020-07-05 05:16:25 +0800

    Add successful module load test

--- src/modules/__tests__/moduleLoader.ts
+++ src/modules/__tests__/moduleLoader.ts
@@ -7,6 +7,20 @@ import { ModuleNotFound, ModuleInternalError } from '../../errors/errors'
 import { stripIndent } from '../../utils/formatters'
 import { createEmptyContext } from '../../createContext'
 
+test('Load a valid module', () => {
+  const path = '_mock_dir/_mock_file'
+  const moduleText = stripIndent`
+    (_params) => {
+      return {
+        hello: 1
+      };
+    }
+  `
+  expect(loadModule(path, createEmptyContext(1, 'default', []), moduleText)).toEqual({
+    hello: 1
+  })
+})
+
 test('Try loading a non-existing module', () => {
   const moduleName = '_non_existing_dir/_non_existing_file'
   expect(() => loadModuleText(moduleName)).toThrow(ModuleNotFound)
