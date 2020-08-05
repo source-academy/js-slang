3eb8aa44734cdaaf29fba60419326d3a7a64ea0d
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2020-08-05 17:58:37 +0800

    Fix test snapshots

--- src/__tests__/display.ts
+++ src/__tests__/display.ts
@@ -2,7 +2,7 @@ import { expectDisplayResult, expectParsedError } from '../utils/testing'
 
 test('display throw error if second argument is non-string when used', () => {
   return expectParsedError(`display(31072020, 0xDEADC0DE);`).toMatchInlineSnapshot(
-    `"Line 1: Error: display expects the second argument to be a string"`
+    `"Line 1: TypeError: display expects the second argument to be a string"`
   )
 })
 
