b9f3332de74d321fa495afe02dd7d42c2c996221
angelsl <angelsl@users.noreply.github.com>
2020-10-01 16:27:54 +0800

    Add EV3 documentation (#777)

--- scripts/jsdoc.sh
+++ scripts/jsdoc.sh
@@ -271,6 +271,14 @@ run() {
 	     -R ${MD}/GAME_README.md \
 	     ${LIB}/game.js 
     
+   # EV3
+
+    ${JSDOC} -r -t ${TMPL} \
+	     -c docs/jsdoc/conf.json \
+	     -d "${DST}/EV3/" \
+	     -R ${MD}/EV3_README.md \
+	     ${LIB}/ev3.js
+
     # External
     
     ${JSDOC} -r -t ${TMPL}/ \
