babe638466a5b2473af31de156a112277ec85dff
Marcus <txk.marcus@gmail.com>
2021-01-28 22:33:32 +0800

    Modified __tests__/moduleLoader.ts to ignore loading non-existing module test

--- src/modules/__tests__/moduleLoader.ts
+++ src/modules/__tests__/moduleLoader.ts
@@ -7,7 +7,7 @@ import { ModuleNotFound, ModuleInternalError } from '../../errors/errors'
 import { stripIndent } from '../../utils/formatters'
 import { createEmptyContext } from '../../createContext'
 
-test('Load a valid module', () => {
+test.only('Load a valid module', () => {
   const path = '_mock_dir/_mock_file'
   const moduleText = stripIndent`
     (_params) => {
@@ -32,7 +32,7 @@ test('Try loading a non-existing module', () => {
   expect(() => loadModuleText(moduleName)).toThrow(ModuleNotFound)
 })
 
-test('Try executing a wrongly implemented module', () => {
+test.only('Try executing a wrongly implemented module', () => {
   // A module in wrong format
   const path = '_mock_dir/_mock_file'
   const wrongModuleText = stripIndent`
