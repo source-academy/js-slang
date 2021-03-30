dd24276aaae3dd17876ef6553f2a7e680ed56c64
Lim Haw Jia <hawjiaa@gmail.com>
2021-03-30 11:21:51 +0800

    Add test for substitution of counters in loop body

--- src/gpu/__tests__/runtimeTranspile.ts
+++ src/gpu/__tests__/runtimeTranspile.ts
@@ -73,7 +73,237 @@ test('gpuRuntimeTranspile replaces external identifiers', () => {
   expect(found).toBe(true)
 })
 
-test.todo('gpuRuntimeTranspile update reference to counters not used as index')
-test.todo('gpuRuntimeTranspile update references to counters used as index')
-test.todo('gpuRuntimeTranspile update references to counters used as index out of order')
-test.todo('gpuRuntimeTranspile update references to counters used as index repeated')
+test('gpuRuntimeTranspile update reference to counters not used as index', () => {
+  const program = `
+    (i, j, k) => {
+      const x = i + j + k;
+      return x;
+    }`
+  const node = (parse(program, { ecmaVersion: 6 }) as unknown) as es.Program
+  const expr = node.body[0] as es.ExpressionStatement
+  const arrowFn = expr.expression as es.ArrowFunctionExpression
+  const localNames = new Set<string>()
+  localNames.add('x')
+  const end = [1, 2, 3]
+  const idx = ['i']
+
+  const res = gpuRuntimeTranspile(arrowFn, localNames, end, idx)
+  let found = false
+  let checkId = (node: es.Node, id: string) => {
+    // this.thread.id
+    if (
+      node.type === 'MemberExpression' &&
+      node.property.type === 'Identifier' &&
+      node.property.name === id &&
+      node.object.type === 'MemberExpression' &&
+      node.object.property.type === 'Identifier' &&
+      node.object.property.name === 'thread' &&
+      node.object.object.type === 'Identifier' &&
+      node.object.object.name === 'this'
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  simple(res, {
+    Identifier(node: es.Identifier) {
+      expect(node.name).not.toEqual('j')
+      expect(node.name).not.toEqual('k')
+    },
+    BinaryExpression(node: es.BinaryExpression) {
+      const left = node.left
+      const right = node.right
+
+      if (
+        // this.thread.x + 2
+        left.type === 'BinaryExpression' &&
+        checkId(left.left, 'x') &&
+        left.right.type === 'Literal' &&
+        left.right.value === 2 &&
+        // 3
+        right.type === 'Literal' &&
+        right.value === 3
+      ) {
+        found = true
+      }
+    }
+  })
+  expect(found).toBe(true)
+})
+
+test('gpuRuntimeTranspile update references to counters used as index', () => {
+  const program = `
+    (i, j, k) => {
+      const x = i + j + k;
+      return x;
+    }`
+  const node = (parse(program, { ecmaVersion: 6 }) as unknown) as es.Program
+  const expr = node.body[0] as es.ExpressionStatement
+  const arrowFn = expr.expression as es.ArrowFunctionExpression
+  const localNames = new Set<string>()
+  localNames.add('x')
+  const end = [1, 2, 3]
+  const idx = ['i', 'j']
+
+  const res = gpuRuntimeTranspile(arrowFn, localNames, end, idx)
+  let found = false
+  let checkId = (node: es.Node, id: string) => {
+    // this.thread.id
+    if (
+      node.type === 'MemberExpression' &&
+      node.property.type === 'Identifier' &&
+      node.property.name === id &&
+      node.object.type === 'MemberExpression' &&
+      node.object.property.type === 'Identifier' &&
+      node.object.property.name === 'thread' &&
+      node.object.object.type === 'Identifier' &&
+      node.object.object.name === 'this'
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  simple(res, {
+    Identifier(node: es.Identifier) {
+      expect(node.name).not.toEqual('i')
+      expect(node.name).not.toEqual('j')
+      expect(node.name).not.toEqual('k')
+    },
+    BinaryExpression(node: es.BinaryExpression) {
+      const left = node.left
+      const right = node.right
+
+      if (
+        // this.thread.y + this.thread.x + 3
+        left.type === 'BinaryExpression' &&
+        checkId(left.left, 'y') &&
+        checkId(left.right, 'x') &&
+        right.type === 'Literal' &&
+        right.value === 3
+      ) {
+        found = true
+      }
+    }
+  })
+  expect(found).toBe(true)
+})
+
+test('gpuRuntimeTranspile update references to counters used as index out of order', () => {
+  const program = `
+    (i, j, k) => {
+      const x = i + j + k;
+      return x;
+    }`
+  const node = (parse(program, { ecmaVersion: 6 }) as unknown) as es.Program
+  const expr = node.body[0] as es.ExpressionStatement
+  const arrowFn = expr.expression as es.ArrowFunctionExpression
+  const localNames = new Set<string>()
+  localNames.add('x')
+  const end = [1, 2, 3]
+  const idx = ['j', 1, 'k', 'i']
+
+  const res = gpuRuntimeTranspile(arrowFn, localNames, end, idx)
+  let found = false
+  let checkId = (node: es.Node, id: string) => {
+    // this.thread.id
+    if (
+      node.type === 'MemberExpression' &&
+      node.property.type === 'Identifier' &&
+      node.property.name === id &&
+      node.object.type === 'MemberExpression' &&
+      node.object.property.type === 'Identifier' &&
+      node.object.property.name === 'thread' &&
+      node.object.object.type === 'Identifier' &&
+      node.object.object.name === 'this'
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  simple(res, {
+    Identifier(node: es.Identifier) {
+      expect(node.name).not.toEqual('i')
+      expect(node.name).not.toEqual('j')
+      expect(node.name).not.toEqual('k')
+    },
+    BinaryExpression(node: es.BinaryExpression) {
+      const left = node.left
+      const right = node.right
+
+      if (
+        // this.thread.x + this.thread.z + this.thread.y
+        left.type === 'BinaryExpression' &&
+        checkId(left.left, 'x') &&
+        checkId(left.right, 'z') &&
+        checkId(right, 'y')
+      ) {
+        found = true
+      }
+    }
+  })
+  expect(found).toBe(true)
+})
+
+test('gpuRuntimeTranspile update references to counters used as index repeated', () => {
+  const program = `
+    (i, j, k) => {
+      const x = i + j + i;
+      return x;
+    }`
+  const node = (parse(program, { ecmaVersion: 6 }) as unknown) as es.Program
+  const expr = node.body[0] as es.ExpressionStatement
+  const arrowFn = expr.expression as es.ArrowFunctionExpression
+  const localNames = new Set<string>()
+  localNames.add('x')
+  const end = [1, 2, 3]
+  const idx = ['i', 'j', 'k']
+
+  const res = gpuRuntimeTranspile(arrowFn, localNames, end, idx)
+  let found = false
+  let checkId = (node: es.Node, id: string) => {
+    // this.thread.id
+    if (
+      node.type === 'MemberExpression' &&
+      node.property.type === 'Identifier' &&
+      node.property.name === id &&
+      node.object.type === 'MemberExpression' &&
+      node.object.property.type === 'Identifier' &&
+      node.object.property.name === 'thread' &&
+      node.object.object.type === 'Identifier' &&
+      node.object.object.name === 'this'
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  simple(res, {
+    Identifier(node: es.Identifier) {
+      expect(node.name).not.toEqual('i')
+      expect(node.name).not.toEqual('j')
+      expect(node.name).not.toEqual('k')
+    },
+    BinaryExpression(node: es.BinaryExpression) {
+      const left = node.left
+      const right = node.right
+
+      if (
+        // this.thread.z + this.thread.y + this.thread.z
+        left.type === 'BinaryExpression' &&
+        checkId(left.left, 'z') &&
+        checkId(left.right, 'y') &&
+        checkId(right, 'z')
+      ) {
+        found = true
+      }
+    }
+  })
+  expect(found).toBe(true)
+})
