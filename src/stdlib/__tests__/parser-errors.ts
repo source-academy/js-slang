61c46927bd1e813dbd4a18d3cf40a2d3e82cd69c
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2021-04-05 04:42:06 +0800

    Format all the parser tests for consistency in line/column numbers

--- src/stdlib/__tests__/parser-errors.ts
+++ src/stdlib/__tests__/parser-errors.ts
@@ -5,7 +5,7 @@ test('Blatant syntax error', () => {
   return expectParsedError(
     stripIndent`
     stringify(parse("'"), undefined, 2);
-  `,
+    `,
     { chapter: 4 }
   ).toMatchInlineSnapshot(`"Line 1: ParseError: SyntaxError: Unterminated string constant (1:0)"`)
 })
@@ -14,7 +14,7 @@ test('Blacklisted syntax', () => {
   return expectParsedError(
     stripIndent`
     stringify(parse("function* f() { yield 1; } f();"), undefined, 2);
-  `,
+    `,
     { chapter: 4 }
   ).toMatchInlineSnapshot(`"Line 1: ParseError: Yield expressions are not allowed"`)
 })
