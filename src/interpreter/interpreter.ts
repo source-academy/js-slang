9d351d5d824c282d6c82da386a46ffe8179b6a00
KarWi <t.karwi@yahoo.com>
2021-04-11 17:22:46 +0800

    Modify context to pass in historically created frames (#908)
    * Modify context runtime to pass in historical frames
    
    - Declare EnvTree and EnvTreeNode classes
    - Map EnvTree object to environmentTree in context runtime
    - Instantiate EnvTree class with globalEnvironment on creation of Runtime object
    
    * Modify pushEnvironment to insert environment into environmentTree
    
    * Add environmentTree property to context runtime object
    
    * Add environmentTree property to context runtime
    
    * Remove isEmptyEnvironment helper
    
    * Add more query operations for EnvTree
    
    * Add more children query operations
    
    * Specify return type of each method explicitly
    
    * Make minor changes
    
    * Modify some access modifiers in EnvNode class
    
    * Add tests for environmentTree
    
    * Make minor changes to jest descriptions
    
    * Update snapshots
    
    * Make minor update to snapshots
    
    * Revert change for defaultBuiltIns::visualiseList
    
    Co-authored-by: martin-henz <henz@comp.nus.edu.sg>

--- src/interpreter/interpreter.ts
+++ src/interpreter/interpreter.ts
@@ -77,7 +77,7 @@ const createEnvironment = (
   return environment
 }
 
-const createBlockEnvironment = (
+export const createBlockEnvironment = (
   context: Context,
   name = 'blockEnvironment',
   head: Frame = {}
@@ -173,8 +173,10 @@ const currentEnvironment = (context: Context) => context.runtime.environments[0]
 const replaceEnvironment = (context: Context, environment: Environment) =>
   (context.runtime.environments[0] = environment)
 const popEnvironment = (context: Context) => context.runtime.environments.shift()
-const pushEnvironment = (context: Context, environment: Environment) =>
+export const pushEnvironment = (context: Context, environment: Environment) => {
   context.runtime.environments.unshift(environment)
+  context.runtime.environmentTree.insert(environment)
+}
 
 const getVariable = (context: Context, name: string) => {
   let environment: Environment | null = context.runtime.environments[0]
--- src/mocks/context.ts
+++ src/mocks/context.ts
@@ -1,8 +1,9 @@
 import * as es from 'estree'
 
-import createContext from '../createContext'
+import createContext, { EnvTree } from '../createContext'
 import Closure from '../interpreter/closure'
-import { Context, Environment, Variant } from '../types'
+import { createBlockEnvironment } from '../interpreter/interpreter'
+import { Context, Environment, Frame, Variant } from '../types'
 
 export function mockContext(chapter = 1, variant: Variant = 'default'): Context {
   return createContext(chapter, variant)
@@ -14,6 +15,7 @@ export function mockRuntimeContext(): Context {
     break: false,
     debuggerOn: true,
     isRunning: true,
+    environmentTree: new EnvTree(),
     environments: [],
     nodes: [
       {
@@ -46,4 +48,12 @@ export function mockClosure(): Closure {
     {} as Environment,
     {} as Context
   )
+}
+
+export function mockEnvironment(
+  context: Context,
+  name = 'blockEnvironment',
+  head: Frame = {}
+): Environment {
+  return createBlockEnvironment(context, name, head)
 }
