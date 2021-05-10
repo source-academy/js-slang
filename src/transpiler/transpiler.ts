7b5babd18a1b15a3e757549dd1d6b7ef18e662e0
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2021-05-10 14:45:31 +0800

    Update dependencies

--- src/transpiler/transpiler.ts
+++ src/transpiler/transpiler.ts
@@ -171,13 +171,19 @@ function createStatementsToStoreCurrentlyDeclaredGlobals(
 ) {
   return program.body
     .filter(statement => statement.type === 'VariableDeclaration')
-    .map(({ declarations: { 0: { id } }, kind }: es.VariableDeclaration) =>
-      createStatementAstToStoreBackCurrentlyDeclaredGlobal(
-        (id as es.Identifier).name,
-        kind as AllowedDeclarations,
-        globalIds,
-        usedIdentifiers
-      )
+    .map(
+      ({
+        declarations: {
+          0: { id }
+        },
+        kind
+      }: es.VariableDeclaration) =>
+        createStatementAstToStoreBackCurrentlyDeclaredGlobal(
+          (id as es.Identifier).name,
+          kind as AllowedDeclarations,
+          globalIds,
+          usedIdentifiers
+        )
     )
 }
 
@@ -477,7 +483,8 @@ export function checkForUndefinedVariablesAndTransformAssignmentsToPropagateBack
            */
           if (!isConstant) {
             // if it is a constant, it will definitely not mutate so above change is not needed
-            const toExpression: es.AssignmentExpression = (identifier as unknown) as es.AssignmentExpression
+            const toExpression: es.AssignmentExpression =
+              identifier as unknown as es.AssignmentExpression
             toExpression.type = 'AssignmentExpression'
             toExpression.operator = '='
             toExpression.left = create.identifier(name)
@@ -732,10 +739,8 @@ export function transpile(
   )
   const statements = program.body as es.Statement[]
   const lastStatement = statements.pop() as es.Statement
-  const {
-    lastStatementStoredInResult,
-    evalMap
-  } = splitLastStatementIntoStorageOfResultAndAccessorPair(lastStatement, globalIds)
+  const { lastStatementStoredInResult, evalMap } =
+    splitLastStatementIntoStorageOfResultAndAccessorPair(lastStatement, globalIds)
 
   const body = [
     wrapWithPreviouslyDeclaredGlobals(
