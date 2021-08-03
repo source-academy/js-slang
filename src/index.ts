ec4cb97ca8abb221f73b622871d9d31d0231a3a4
Joey Chen <chen.kangaroo@gmail.com>
2021-08-03 16:52:46 +0800

    Added tests and minor fixes (#1081)

--- src/index.ts
+++ src/index.ts
@@ -473,13 +473,12 @@ export async function runInContext(
     })
   }
   const isNativeRunnable = determineExecutionMethod(theOptions, context, program)
-  if (context.nativeStorage.evaller === null || context.numberOfOuterEnvironments)
-    if (context.prelude !== null) {
-      const prelude = context.prelude
-      context.prelude = null
-      await runInContext(prelude, context, { ...options, isPrelude: true })
-      return runInContext(code, context, options)
-    }
+  if (context.prelude !== null) {
+    const prelude = context.prelude
+    context.prelude = null
+    await runInContext(prelude, context, { ...options, isPrelude: true })
+    return runInContext(code, context, options)
+  }
   if (isNativeRunnable) {
     if (previousCode === code && isPreviousCodeTimeoutError) {
       context.nativeStorage.maxExecTime *= JSSLANG_PROPERTIES.factorToIncreaseBy
