ac62c14cbf96ac9acbe40a3aa42b4f1f84fce84c
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2021-08-25 15:20:40 +0800

    Add `tokenize` to Source chapter 4

---
+++ src/parser/__tests__/tokenize.ts
@@ -0,0 +1,90 @@
+import { stripIndent } from '../../utils/formatters'
+import { expectDisplayResult, expectParsedError } from '../../utils/testing'
+
+test('tokenize works for a good program', () => {
+  return expectDisplayResult(
+    'display_list(tokenize(' +
+      JSON.stringify(stripIndent`
+      function f(x) {
+        const y = x + x + x + "123";
+        return z => (a, b) => {
+          let w = z + 1;
+          return y;
+        };
+      }
+      f("55");
+      `) +
+      '));',
+    { chapter: 4 }
+  ).toMatchInlineSnapshot(`
+            Array [
+              "list(\\"function\\",
+                 \\"f\\",
+                 \\"(\\",
+                 \\"x\\",
+                 \\")\\",
+                 \\"{\\",
+                 \\"const\\",
+                 \\"y\\",
+                 \\"=\\",
+                 \\"x\\",
+                 \\"+\\",
+                 \\"x\\",
+                 \\"+\\",
+                 \\"x\\",
+                 \\"+\\",
+                 \\"\\\\\\"123\\\\\\"\\",
+                 \\";\\",
+                 \\"return\\",
+                 \\"z\\",
+                 \\"=>\\",
+                 \\"(\\",
+                 \\"a\\",
+                 \\",\\",
+                 \\"b\\",
+                 \\")\\",
+                 \\"=>\\",
+                 \\"{\\",
+                 \\"let\\",
+                 \\"w\\",
+                 \\"=\\",
+                 \\"z\\",
+                 \\"+\\",
+                 \\"1\\",
+                 \\";\\",
+                 \\"return\\",
+                 \\"y\\",
+                 \\";\\",
+                 \\"}\\",
+                 \\";\\",
+                 \\"}\\",
+                 \\"f\\",
+                 \\"(\\",
+                 \\"\\\\\\"55\\\\\\"\\",
+                 \\")\\",
+                 \\";\\")",
+            ]
+          `)
+})
+
+test('tokenize works even with parse errors', () => {
+  return expectDisplayResult(
+    'display_list(tokenize(' +
+      JSON.stringify(stripIndent`
+      function f(x) {
+      ;;;;;;;
+      `) +
+      '));',
+    { chapter: 4 }
+  ).toMatchInlineSnapshot(`
+            Array [
+              "list(\\"function\\", \\"f\\", \\"(\\", \\"x\\", \\")\\", \\"{\\", \\";\\", \\";\\", \\";\\", \\";\\", \\";\\", \\";\\", \\";\\")",
+            ]
+          `)
+})
+
+test('tokenize prints suitable error when tokenization fails', () => {
+  return expectParsedError('display_list(tokenize("\\""));', { chapter: 4 }).toMatchInlineSnapshot(
+    `"Line 1: SyntaxError: Unterminated string constant (1:0)"`
+  )
+})
