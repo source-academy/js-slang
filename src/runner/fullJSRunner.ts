55ad2d6f82bebdbf4681c000947f4d1deaf02ca7
leeyi45 <leeyi45@gmail.com>
2022-02-26 18:47:17 +0800

    Update fullJSRunner.ts
    Allow the fullJS runner to import modules with their contexts, and also handle import errors

--- src/runner/fullJSRunner.ts
+++ src/runner/fullJSRunner.ts
@@ -2,9 +2,11 @@
 import { Options, parse } from 'acorn'
 import { generate } from 'astring'
 import * as es from 'estree'
+import { hoistImportDeclarations } from '.'
 
-import { IOptions, Result } from '..'
+import { IOptions, ModuleContext, Result } from '..'
 import { NATIVE_STORAGE_ID } from '../constants'
+import { RuntimeSourceError } from '../errors/runtimeSourceError'
 import { FatalSyntaxError } from '../parser/parser'
 import { evallerReplacer, getBuiltins, prefixModule, transpile } from '../transpiler/transpiler'
 import { Context } from '../types'
@@ -45,7 +47,7 @@ function parseFullJS(code: string, context: Context): es.Program | undefined {
   return program
 }
 
-function fullJSEval(code: string, nativeStorage: NativeStorage, moduleParams: any): any {
+function fullJSEval(code: string, nativeStorage: NativeStorage, moduleParams: any, moduleContexts: Map<string, ModuleContext>): any {
   if (nativeStorage.evaller) {
     return nativeStorage.evaller(code)
   } else {
@@ -75,6 +77,8 @@ export async function fullJSRunner(
     return resolvedErrorPromise
   }
 
+  hoistImportDeclarations(program);
+
   // prelude & builtins
   // TODO resolve repeated declaration in prelude and builtins
   context.nativeStorage.builtins.delete('list_to_stream')
@@ -85,19 +89,28 @@ export async function fullJSRunner(
   ]
 
   // modules
-  appendModulesToContext(program, context)
+  try {
+    appendModulesToContext(program, context)
+  } catch (error) {
+    if (error instanceof RuntimeSourceError) {
+      context.errors.push(error);
+      return resolvedErrorPromise;
+    }
+    throw error;
+  };
+
   const modulePrefix: string = prefixModule(program)
 
   const preEvalProgram: es.Program = create.program(preludeBuiltInStatements)
   const preEvalCode: string = generate(preEvalProgram) + modulePrefix
-  await fullJSEval(preEvalCode, context.nativeStorage, options)
+  await fullJSEval(preEvalCode, context.nativeStorage, options, context.moduleContexts)
 
   const { transpiled, sourceMapJson } = transpile(program, context)
   try {
     return Promise.resolve({
       status: 'finished',
       context,
-      value: await fullJSEval(transpiled, context.nativeStorage, options)
+      value: await fullJSEval(transpiled, context.nativeStorage, options, context.moduleContexts)
     })
   } catch (error) {
     context.errors.push(await toSourceError(error, sourceMapJson))
