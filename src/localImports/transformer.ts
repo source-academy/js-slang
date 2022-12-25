dfdc3557015969ece1088ad01100f417e5e87147
Ian Yong <ianyongyc@gmail.com>
2022-12-25 22:58:02 +0800

    Move type guards into a separate file for organisation

--- src/localImports/transformer.ts
+++ src/localImports/transformer.ts
@@ -7,41 +7,7 @@ import {
   createLiteral,
   createPairCallExpression
 } from './constructors'
-
-// It is necessary to write this type guard like this as the 'type' of both
-// 'Directive' & 'ExpressionStatement' is 'ExpressionStatement'.
-//
-// export interface Directive extends BaseNode {
-//   type: "ExpressionStatement";
-//   expression: Literal;
-//   directive: string;
-// }
-//
-// export interface ExpressionStatement extends BaseStatement {
-//   type: "ExpressionStatement";
-//   expression: Expression;
-// }
-//
-// As such, we check whether the 'directive' property exists on the object
-// instead in order to differentiate between the two.
-const isDirective = (node: es.Node): node is es.Directive => {
-  return 'directive' in node
-}
-
-const isModuleDeclaration = (node: es.Node): node is es.ModuleDeclaration => {
-  return [
-    'ImportDeclaration',
-    'ExportNamedDeclaration',
-    'ExportDefaultDeclaration',
-    'ExportAllDeclaration'
-  ].includes(node.type)
-}
-
-const isStatement = (
-  node: es.Directive | es.Statement | es.ModuleDeclaration
-): node is es.Statement => {
-  return !isDirective(node) && !isModuleDeclaration(node)
-}
+import { isDirective, isModuleDeclaration, isStatement } from './typeGuards'
 
 const getExportedName = (node: es.Declaration): string | null => {
   switch (node.type) {
