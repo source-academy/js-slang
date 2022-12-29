db1e8fb6039a521e0fbf8419af232e572333f0c1
Ian Yong <ianyongyc@gmail.com>
2022-12-29 23:18:29 +0800

    Add test case when entrypoint file does not contain imports/exports

--- src/localImports/__tests__/preprocessor.ts
+++ src/localImports/__tests__/preprocessor.ts
@@ -1,19 +1,54 @@
+import es from 'estree'
+
 import { mockContext } from '../../mocks/context'
+import { parse } from '../../parser/parser'
 import { Chapter } from '../../types'
 import preprocessFileImports from '../preprocessor'
+import { stripLocationInfo } from './utils'
 
 describe('preprocessFileImports', () => {
-  let context = mockContext(Chapter.SOURCE_2)
+  const parseError = new Error('Unable to parse code')
+  let actualContext = mockContext(Chapter.SOURCE_2)
+  let expectedContext = mockContext(Chapter.SOURCE_2)
 
   beforeEach(() => {
-    context = mockContext(Chapter.SOURCE_2)
+    actualContext = mockContext(Chapter.SOURCE_2)
+    expectedContext = mockContext(Chapter.SOURCE_2)
   })
 
-  it('returns undefined if the specified entrypoint file does not exist', () => {
+  const assertASTsAreEquivalent = (
+    actualProgram: es.Program | undefined,
+    expectedCode: string
+  ): void => {
+    if (actualProgram === undefined) {
+      throw parseError
+    }
+
+    const expectedProgram = parse(expectedCode, expectedContext)
+    if (expectedProgram === undefined) {
+      throw parseError
+    }
+
+    expect(stripLocationInfo(actualProgram)).toEqual(stripLocationInfo(expectedProgram))
+  }
+
+  it('returns undefined if the entrypoint file does not exist', () => {
     const files: Record<string, string> = {
       'a.js': '1 + 2;'
     }
-    const program = preprocessFileImports(files, 'non-existent-file.js', context)
+    const program = preprocessFileImports(files, 'non-existent-file.js', actualContext)
     expect(program).toBeUndefined()
+  })
+
+  it('returns the same AST if the entrypoint file does not contain import/export statements', () => {
+    const files: Record<string, string> = {
+      'a.js': `function square(x) {
+                return x * x;
+              }
+              square(5);
+              `
+    }
+    const program = preprocessFileImports(files, 'a.js', actualContext)
+    assertASTsAreEquivalent(program, files['a.js'])
   })
 })
