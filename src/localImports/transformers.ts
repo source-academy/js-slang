32b47dbb95e5c815026f8d294c878bd1ef3ded8a
Ian Yong <ianyongyc@gmail.com>
2022-12-31 22:35:33 +0800

    Add header comment to removeNonSourceModuleImports

--- src/localImports/transformers.ts
+++ src/localImports/transformers.ts
@@ -270,6 +270,22 @@ export const isSourceModule = (value: string): boolean => {
   return !isFilePath(value)
 }
 
+/**
+ * Removes all non-Source module import-related nodes from the AST.
+ *
+ * All import-related nodes which are not removed in the pre-processing
+ * step will be treated by the Source modules loader as a Source module.
+ * If a Source module by the same name does not exist, the program
+ * evaluation will error out. As such, this function removes all
+ * import-related AST nodes which the Source module loader does not
+ * support, as well as ImportDeclaration nodes for local module imports.
+ *
+ * The definition of whether a module is a local module or a Source
+ * module depends on the implementation of the `isSourceModule` function.
+ *
+ * @param program The AST which should be stripped of non-Source module
+ *                import-related nodes.
+ */
 export const removeNonSourceModuleImports = (program: es.Program): void => {
   // First pass: remove all import AST nodes which are unused by Source modules.
   ancestor(program, {
