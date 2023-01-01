3c47a9a7a16dbc446901f33e339ec1c86d47c588
Ian Yong <ianyongyc@gmail.com>
2023-01-01 17:43:27 +0800

    Add localImportPrelude with named export accessor

---
+++ src/stdlib/localImport.prelude.ts
@@ -0,0 +1,20 @@
+export const localImportPrelude = `
+function __access_named_export__(named_exports, lookup_name) {
+  if (is_null(named_exports)) {
+    return undefined;
+  } else {
+    const name = head(head(named_exports));
+    const identifier = tail(head(named_exports));
+    if (name === lookup_name) {
+      return identifier;
+    } else {
+      return __access_named_export__(tail(named_exports), lookup_name);
+    }
+  }
+}
+
+function __access_export__(exports, lookup_name) {
+  const named_exports = tail(exports);
+  return __access_named_export__(named_exports, lookup_name);
+}
+`
