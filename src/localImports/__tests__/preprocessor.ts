8a01dfa2fe806a6ae77736a495609c0ec4f69161
Ian Yong <ianyongyc@gmail.com>
2023-01-01 23:32:49 +0800

    Add test cases for getImportedLocalModulePaths

--- src/localImports/__tests__/preprocessor.ts
+++ src/localImports/__tests__/preprocessor.ts
@@ -3,8 +3,75 @@ import es from 'estree'
 import { mockContext } from '../../mocks/context'
 import { parse } from '../../parser/parser'
 import { Chapter } from '../../types'
-import preprocessFileImports from '../preprocessor'
+import preprocessFileImports, { getImportedLocalModulePaths } from '../preprocessor'
 import { parseError, stripLocationInfo } from './utils'
+
+describe('getImportedLocalModulePaths', () => {
+  let context = mockContext(Chapter.LIBRARY_PARSER)
+
+  beforeEach(() => {
+    context = mockContext(Chapter.LIBRARY_PARSER)
+  })
+
+  const assertCorrectModulePathsAreReturned = (
+    code: string,
+    baseFilePath: string,
+    expectedModulePaths: string[]
+  ): void => {
+    const program = parse(code, context)
+    if (program === undefined) {
+      throw parseError
+    }
+    expect(getImportedLocalModulePaths(program, baseFilePath)).toEqual(new Set(expectedModulePaths))
+  }
+
+  it('throws an error if the current file path is not absolute', () => {
+    const code = ''
+    const program = parse(code, context)
+    if (program === undefined) {
+      throw parseError
+    }
+    expect(() => getImportedLocalModulePaths(program, 'a.js')).toThrowError(
+      "Current file path 'a.js' is not absolute."
+    )
+  })
+
+  it('returns local (relative) module imports', () => {
+    const code = `import { x } from "./dir2/b.js";
+      import { y } from "../dir3/c.js";
+    `
+    assertCorrectModulePathsAreReturned(code, '/dir/a.js', ['/dir/dir2/b.js', '/dir3/c.js'])
+  })
+
+  it('returns local (absolute) module imports', () => {
+    const code = `import { x } from "/dir/dir2/b.js";
+      import { y } from "/dir3/c.js";
+    `
+    assertCorrectModulePathsAreReturned(code, '/dir/a.js', ['/dir/dir2/b.js', '/dir3/c.js'])
+  })
+
+  it('does not return Source module imports', () => {
+    const code = `import { x } from "rune";
+      import { y } from "sound";
+    `
+    assertCorrectModulePathsAreReturned(code, '/dir/a.js', [])
+  })
+
+  it('gracefully handles overly long sequences of double dots (..)', () => {
+    const code = `import { x } from "../../../../../../../../../b.js";
+    `
+    assertCorrectModulePathsAreReturned(code, '/dir/a.js', ['/b.js'])
+  })
+
+  it('returns unique module paths', () => {
+    const code = `import { a } from "./b.js";
+      import { b } from "./b.js";
+      import { c } from "./c.js";
+      import { d } from "./c.js";
+    `
+    assertCorrectModulePathsAreReturned(code, '/dir/a.js', ['/dir/b.js', '/dir/c.js'])
+  })
+})
 
 describe('preprocessFileImports', () => {
   let actualContext = mockContext(Chapter.LIBRARY_PARSER)
