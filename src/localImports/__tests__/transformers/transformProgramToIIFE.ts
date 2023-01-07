4725d2006a2389d4467313de29941da4252abefa
Ian Yong <ianyongyc@gmail.com>
2023-01-07 19:20:53 +0800

    Add test case for limiting resolved file paths to the file system root

--- src/localImports/__tests__/transformers/transformProgramToIIFE.ts
+++ src/localImports/__tests__/transformers/transformProgramToIIFE.ts
@@ -313,4 +313,19 @@ describe('transformImportedFile', () => {
     `
     assertASTsAreEquivalent(actualCode, expectedCode)
   })
+
+  it('limits resolved file paths to the root of the file system `/`', () => {
+    const actualCode = `
+      import { x } from "../../../../../../../../../dir/b.js";
+      import { y } from "../../../../../dir2/c.js";
+    `
+    const expectedCode = `
+      function ${iifeIdentifier}(__$dir$b$dot$js__, __$dir2$c$dot$js__) {
+        const x = __access_export__(__$dir$b$dot$js__, "x");
+        const y = __access_export__(__$dir2$c$dot$js__, "y");
+        return pair(null, list());
+      }
+    `
+    assertASTsAreEquivalent(actualCode, expectedCode)
+  })
 })
