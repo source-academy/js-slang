0157055ce3f899fd9468712419bb9ecf99a6627c
Ian Yong <ianyongyc@gmail.com>
2023-01-07 23:59:22 +0800

    Represent default export with 'default' instead of null

--- src/stdlib/localImport.prelude.ts
+++ src/stdlib/localImport.prelude.ts
@@ -1,4 +1,9 @@
 export const accessExportFunctionName = '__access_export__'
+// We can make use of 'default' to denote that the default export
+// should be accessed because 'default' is a reserved keyword in
+// Source. Specifically, the Acorn parser does not allow 'default'
+// to be used as a name.
+export const defaultExportLookupName = 'default'
 
 export const localImportPrelude = `
 function __access_named_export__(named_exports, lookup_name) {
@@ -16,7 +21,7 @@ function __access_named_export__(named_exports, lookup_name) {
 }
 
 function ${accessExportFunctionName}(exports, lookup_name) {
-  if (is_null(lookup_name)) {
+  if (lookup_name === "${defaultExportLookupName}") {
     return head(exports);
   } else {
     const named_exports = tail(exports);
