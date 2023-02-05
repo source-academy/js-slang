0ad45d8ff9b935c2ac04276926b4e3a986d26713
Ian Yong <ianyongyc@gmail.com>
2023-02-05 17:20:48 +0800

    Add test cases for typed Source

--- src/localImports/__tests__/errorMessages.ts
+++ src/localImports/__tests__/errorMessages.ts
@@ -1,6 +1,6 @@
 import { parseError, runFilesInContext } from '../../index'
 import { mockContext } from '../../mocks/context'
-import { Chapter } from '../../types'
+import { Chapter, Variant } from '../../types'
 
 describe('syntax errors', () => {
   let context = mockContext(Chapter.SOURCE_4)
@@ -128,6 +128,45 @@ describe('non-syntax errors (non-transpiled)', () => {
       await runFilesInContext(files, '/a.js', context)
       expect(parseError(context.errors)).toMatchInlineSnapshot(
         `"[/a.js] Line 2: Expected number on right hand side of operation, got string."`
+      )
+    })
+  })
+})
+
+// We specifically test typed Source because it makes use of the Babel parser.
+describe('non-syntax errors (non-transpiled & typed)', () => {
+  let context = mockContext(Chapter.SOURCE_4, Variant.TYPED)
+
+  beforeEach(() => {
+    context = mockContext(Chapter.SOURCE_4, Variant.TYPED)
+    context.executionMethod = 'interpreter'
+  })
+
+  describe('SourceError', () => {
+    test('file path is not part of error message if the program is single-file', async () => {
+      const files: Record<string, string> = {
+        '/a.js': `
+          2 + 'hello';
+        `
+      }
+      await runFilesInContext(files, '/a.js', context)
+      expect(parseError(context.errors)).toMatchInlineSnapshot(
+        `"Line 2: Type 'string' is not assignable to type 'number'."`
+      )
+    })
+
+    test('file path is part of error message if the program is multi-file', async () => {
+      const files: Record<string, string> = {
+        '/a.js': `
+          2 + 'hello';
+        `,
+        '/b.js': `
+          const y = 2;
+        `
+      }
+      await runFilesInContext(files, '/a.js', context)
+      expect(parseError(context.errors)).toMatchInlineSnapshot(
+        `"[/a.js] Line 2: Type 'string' is not assignable to type 'number'."`
       )
     })
   })
