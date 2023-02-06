163d0caecc3dfd469ec52e47f6be690c3ce1da33
shenyihong <shen_yi_hong@outlook.com>
2023-02-06 22:54:21 +0800

    refactor: source (variant: `typed`) parser class

---
+++ src/parsers/source/typed/index.ts
@@ -0,0 +1,26 @@
+import { parse as babelParse, ParserOptions as babelOptions } from '@babel/parser'
+import { Program } from 'estree'
+
+import { Context } from '../../..'
+import * as TypedES from '../../../typeChecker/tsESTree'
+import { checkForTypeErrors } from '../../../typeChecker/typeErrorChecker'
+import { SourceParser } from '..'
+import TypeParser from './typeParser'
+
+export class SourceTypedParser extends SourceParser {
+  static defaultBabelOptions: babelOptions = {
+    sourceType: 'module',
+    plugins: ['typescript', 'estree']
+  }
+
+  parse(programStr: string, context: Context, _throwOnError?: boolean): Program | null {
+    TypeParser.parse(programStr, SourceParser.defaultAcornOptions)
+
+    const typedProgram: TypedES.Program = babelParse(
+      programStr,
+      SourceTypedParser.defaultBabelOptions
+    ).program as unknown as TypedES.Program
+
+    return checkForTypeErrors(typedProgram, context)
+  }
+}
