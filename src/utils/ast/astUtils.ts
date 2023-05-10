f7eea1330258b9c50520cd02c5b03da3d2ac7b42
Lee Yi <leeyi45@gmail.com>
2023-05-10 12:59:06 +0800

    Add new convenience method for defaiult exports

--- src/utils/ast/astUtils.ts
+++ src/utils/ast/astUtils.ts
@@ -1,4 +1,6 @@
-import type * as es from 'estree'
+import * as es from 'estree'
+import assert from '../assert'
+import { isDeclaration } from './typeGuards'
 
 import { recursive } from './walkers'
 
@@ -19,4 +21,45 @@ export function extractIdsFromPattern(pattern: es.Pattern): Set<es.Identifier> {
     RestElement: ({ argument }: es.RestElement, _state, c) => c(argument, null)
   })
   return ids
+}
+
+type Processors<T> = {
+  FunctionDeclaration: (node: es.FunctionDeclaration) => T
+  ClassDeclaration: (node: es.ClassDeclaration) => T
+  Expression: (node: es.Expression) => T
+}
+
+export function processExportDefaultDeclaration<T>(
+  node: es.ExportDefaultDeclaration,
+  processors: Processors<T>
+) {
+  if (isDeclaration(node.declaration)) {
+    const declaration = node.declaration
+    assert(
+      declaration.type !== 'VariableDeclaration',
+      'ExportDefaultDeclarations cannot have VariableDeclarations'
+    )
+
+    if (declaration.type === 'FunctionDeclaration') {
+      if (declaration.id) {
+        return processors.FunctionDeclaration(declaration)
+      }
+
+      return processors.Expression({
+        ...declaration,
+        type: 'FunctionExpression'
+      })
+    }
+
+    if (declaration.id) {
+      return processors.ClassDeclaration(declaration)
+    }
+
+    return processors.Expression({
+      ...declaration,
+      type: 'ClassExpression'
+    })
+  }
+
+  return processors.Expression(node.declaration)
 }
