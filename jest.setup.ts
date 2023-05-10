9397c9092d01269f08c5e9d680db2882692e402e
Lee Yi <leeyi45@gmail.com>
2023-05-10 12:59:24 +0800

    Fix SourceMapConsumer not working with tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,5 @@
+import { SourceMapConsumer } from "source-map"
+
 jest.mock('lodash', () => ({
   ...jest.requireActual('lodash'),
   memoize: jest.fn((x: any) => x),
@@ -6,3 +8,10 @@ jest.mock('lodash', () => ({
 
 jest.mock('./src/modules/moduleLoaderAsync')
 jest.mock('./src/modules/moduleLoader')
+
+// @ts-ignore
+SourceMapConsumer.initialize({
+  'lib/mappings.wasm': 'https://unpkg.com/source-map@0.7.3/lib/mappings.wasm'
+})
+
+global.fetch = jest.fn()
