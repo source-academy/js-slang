a032cbb9bc969f2236c2c8ea932d70b9af8cc2f6
Lee Yi <leeyi45@gmail.com>
2023-05-18 11:28:03 +0800

    Use base walkers provided in acorn-walk

--- src/utils/ast/astUtils.ts
+++ src/utils/ast/astUtils.ts
@@ -2,25 +2,43 @@ import * as es from 'estree'
 
 import assert from '../assert'
 import { isDeclaration } from './typeGuards'
-import { recursive } from './walkers'
+import { simple } from './walkers'
 
 export function extractIdsFromPattern(pattern: es.Pattern): Set<es.Identifier> {
   const ids = new Set<es.Identifier>()
-  recursive(pattern, null, {
-    ArrayPattern: ({ elements }: es.ArrayPattern, _state, c) =>
-      elements.forEach(elem => {
-        if (elem) c(elem, null)
-      }),
-    AssignmentPattern: (p: es.AssignmentPattern, _state, c) => {
-      c(p.left, null)
-      c(p.right, null)
-    },
+  if (pattern.type === 'MemberExpression') return ids
+
+  simple(pattern, {
     Identifier: (id: es.Identifier) => ids.add(id),
-    ObjectPattern: ({ properties }: es.ObjectPattern, _state, c) =>
-      properties.forEach(prop => c(prop, null)),
-    RestElement: ({ argument }: es.RestElement, _state, c) => c(argument, null)
   })
   return ids
+  // recursive(pattern, null, {
+  //   ArrayPattern: ({ elements }: es.ArrayPattern, _state, c) =>
+  //     elements.forEach(elem => {
+  //       if (elem) c(elem, null)
+  //     }),
+  //   AssignmentPattern: (p: es.AssignmentPattern, _state, c) => {
+  //     c(p.left, null)
+  //     c(p.right, null)
+  //   },
+  //   Identifier: (id: es.Identifier) => ids.add(id),
+  //   ObjectPattern: ({ properties }: es.ObjectPattern, _state, c) =>
+  //     properties.forEach(prop => c(prop, null)),
+  //   RestElement: ({ argument }: es.RestElement, _state, c) => c(argument, null)
+  // })
+  // return ids
+}
+
+export function declarationToExpression(node: es.ClassDeclaration): es.ClassExpression
+export function declarationToExpression(node: es.FunctionDeclaration): es.FunctionExpression
+export function declarationToExpression({
+  type,
+  ...node
+}: es.FunctionDeclaration | es.ClassDeclaration): es.FunctionExpression | es.ClassExpression {
+  return {
+    ...node,
+    type: type === 'FunctionDeclaration' ? 'FunctionExpression' : 'ClassExpression'
+  } as es.FunctionExpression | es.ClassExpression
 }
 
 type Processors<T> = {
