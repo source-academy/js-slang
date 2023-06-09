187850681984a8f3ad5a66793d7ae17a45600a45
Lee Yi <leeyi45@gmail.com>
2023-06-09 16:42:08 -0700

    Add new AST helper types

--- src/utils/ast/types.ts
+++ src/utils/ast/types.ts
@@ -1,6 +1,12 @@
+// Reexport the type definitions from estree, but with several helper types as well
+
 import type * as es from 'estree'
 
 export type ExportDeclaration = Exclude<es.ModuleDeclaration, es.ImportDeclaration>
+export type ImportSpecifiers =
+  | es.ImportSpecifier
+  | es.ImportDefaultSpecifier
+  | es.ImportNamespaceSpecifier
 export type SourcedModuleDeclaration = Exclude<es.ModuleDeclaration, es.ExportDefaultDeclaration>
 
 export interface BlockArrowFunctionExpression
