102a3b04edb4da2120aa62d91e8617945db71a56
shenyihong <shen_yi_hong@outlook.com>
2024-03-10 21:41:32 +0800

    refactor: move peg grammar file dedicated `go-slang` folder

--- src/parser/go/index.ts
+++ src/parser/go/index.ts
@@ -1,6 +1,6 @@
 import { FatalSyntaxError } from '../errors'
 import { Parser } from '../types'
-import { parse } from './go.js'
+import { parse } from '../../go-slang/parser/go.js'
 
 export class GoParser implements Parser<any> {
   parse(programStr: string, context: any, options?: any, throwOnError?: boolean): any {
@@ -20,6 +20,8 @@ export class GoParser implements Parser<any> {
       if (throwOnError) throw error
       context.errors.push(error)
     }
+
+    return null
   }
   validate(ast: any, context: any, throwOnError?: boolean): boolean {
     return true
