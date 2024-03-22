1b9fe78ce5c9911630f2e5be7b3ac878bac3cbc3
CZX <CZX123@users.noreply.github.com>
2024-03-22 18:38:30 +0800

    Remove semicolons

--- src/cse-machine/heap.ts
+++ src/cse-machine/heap.ts
@@ -1,33 +1,33 @@
-import { HeapObject } from './types';
+import { HeapObject } from './types'
 
 /**
  * The heap stores all objects in each environment.
  */
 export default class Heap {
-  private storage: Set<HeapObject> | null = null;
+  private storage: Set<HeapObject> | null = null
 
-  public constructor() { }
+  public constructor() {}
 
   add(...items: HeapObject[]): void {
     if (!this.storage) {
-      this.storage = new Set<HeapObject>(items);
+      this.storage = new Set<HeapObject>(items)
     } else {
       for (const item of items) {
-        this.storage.add(item);
+        this.storage.add(item)
       }
     }
   }
 
   contains(item: HeapObject): boolean {
-    return this.storage?.has(item) ?? false;
+    return this.storage?.has(item) ?? false
   }
 
   size(): number {
-    return this.storage?.size ?? 0;
+    return this.storage?.size ?? 0
   }
 
   getHeap(): Set<HeapObject> {
     // return a copy of the heap's contents
-    return new Set(this.storage);
+    return new Set(this.storage)
   }
 }
