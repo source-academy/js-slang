95c5e7d41cf5451593c73c230db1cfa4fce51a20
Dang Hoang Duy <e1129930@u.nus.edu>
2024-04-27 22:28:59 +0800

    Cache isEnvDependent results

--- src/cse-machine/types.ts
+++ src/cse-machine/types.ts
@@ -93,7 +93,13 @@ export type Instr =
   | GenContInstr
   | ResumeContInstr
 
-export type ControlItem = Node | Instr
+export type ControlItem = (Node | Instr) & {
+  isEnvDependent?: boolean
+}
+// {
+//   body: Node | Instr
+//   isEnvDependent: boolean
+// }
 
 // Every array also has the properties `id` and `environment` for use in the frontend CSE Machine
 export type EnvArray = any[] & {
