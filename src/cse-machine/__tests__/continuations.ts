bd617e663c2caeedf8d06d6265cc0777b1499910
Kyriel Abad <abadkyriel@gmail.com>
2024-07-18 13:52:16 +0800

    Improve testing for continuations

--- src/cse-machine/__tests__/continuations.ts
+++ src/cse-machine/__tests__/continuations.ts
@@ -1,5 +1,20 @@
-import { Call_cc } from '../continuations'
+import { Call_cc, Continuation, isCallWithCurrentContinuation } from '../continuations'
+import { Control, Stash } from '../interpreter'
 
-test('Call/cc is a singleton', () => {
+test('call/cc is a singleton', () => {
   expect(Call_cc.get()).toBe(Call_cc.get())
+})
+
+test('call/cc toString', () => {
+  expect(Call_cc.get().toString()).toBe('call/cc')
+})
+
+test('isCallWithCurrentContinuation works on call/cc only', () => {
+  expect(isCallWithCurrentContinuation(Call_cc.get())).toBe(true)
+  expect(isCallWithCurrentContinuation(1)).toBe(false)
+})
+
+test('Continuation toString', () => {
+  const cont = new Continuation(new Control(), new Stash(), [])
+  expect(cont.toString()).toBe('continuation')
 })
