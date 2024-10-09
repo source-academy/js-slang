bbcabab453a735b7c19cec5f98b3c27a00411d30
DESKTOP-G08HS3B\Lee Yi <leeyi45@gmail.com>
2024-10-08 23:44:09 -0400

    Remove non-det

--- src/repl/index.ts
+++ src/repl/index.ts
@@ -3,11 +3,9 @@
 import { Command } from '@commander-js/extra-typings'
 
 import { getReplCommand } from './repl'
-import { nonDetCommand } from './repl-non-det'
 import { transpilerCommand } from './transpiler'
 
 new Command()
   .addCommand(transpilerCommand)
   .addCommand(getReplCommand(), { isDefault: true })
-  .addCommand(nonDetCommand)
   .parseAsync()
