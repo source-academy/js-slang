a06bb4302f54a853346733dfefce680785017d16
Lee Yi <leeyi45@gmail.com>
2024-10-11 12:02:55 -0400

    Add explanation for ts-expect-error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@
 import tseslint from 'typescript-eslint'
 import globals from 'globals'
 
-// @ts-expect-error
+// @ts-expect-error no type definitions for plugin
 import importPlugin from 'eslint-plugin-import'
 
 export default tseslint.config(
