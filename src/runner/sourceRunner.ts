e35223ffb2e406cce58520cc968ea08440663aa1
Lee Yi <leeyi45@gmail.com>
2025-03-03 00:58:02 -0500

    Remove lazy (#1743)
    * Remove lazy
    
    * Run format
    
    * Utilize yarn 4.6.0
    
    * Remove GPU
    
    * Remove gpu.js
    
    ---------
    
    Co-authored-by: Martin Henz <henz@comp.nus.edu.sg>

--- src/runner/sourceRunner.ts
+++ src/runner/sourceRunner.ts
@@ -8,11 +8,9 @@ import { CSEResultPromise, evaluate as CSEvaluate } from '../cse-machine/interpr
 import { ExceptionError } from '../errors/errors'
 import { RuntimeSourceError } from '../errors/runtimeSourceError'
 import { TimeoutError } from '../errors/timeoutErrors'
-import { transpileToGPU } from '../gpu/gpu'
 import { isPotentialInfiniteLoop } from '../infiniteLoops/errors'
 import { testForInfiniteLoop } from '../infiniteLoops/runtime'
 import { evaluateProgram as evaluate } from '../interpreter/interpreter'
-import { transpileToLazy } from '../lazy/lazy'
 import preprocessFileImports from '../modules/preprocessor'
 import { defaultAnalysisOptions } from '../modules/preprocessor/analyzer'
 import { defaultLinkerOptions } from '../modules/preprocessor/linker'
@@ -28,7 +26,6 @@ import {
 import { sandboxedEval } from '../transpiler/evalContainer'
 import { transpile } from '../transpiler/transpiler'
 import { Chapter, type Context, type RecursivePartial, type Scheduler, Variant } from '../types'
-import { forceIt } from '../utils/operators'
 import { validateAndAnnotate } from '../validator/validator'
 import { compileForConcurrent } from '../vm/svml-compiler'
 import { runWithProgram } from '../vm/svml-machine'
@@ -145,21 +142,8 @@ async function runNative(
   let transpiled
   let sourceMapJson: RawSourceMap | undefined
   try {
-    switch (context.variant) {
-      case Variant.GPU:
-        transpileToGPU(transpiledProgram)
-        break
-      case Variant.LAZY:
-        transpileToLazy(transpiledProgram)
-        break
-    }
-
     ;({ transpiled, sourceMapJson } = transpile(transpiledProgram, context))
     let value = sandboxedEval(transpiled, context.nativeStorage)
-
-    if (context.variant === Variant.LAZY) {
-      value = forceIt(value)
-    }
 
     if (!options.isPrelude) {
       isPreviousCodeTimeoutError = false
