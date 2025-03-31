3d71c2769d62dcca36b00b850e36d2f5b9b8a228
Kosolpattanadurong Thitiwat <thitiwatipst@icloud.com>
2025-03-31 16:41:30 +0800

    feat: add predeclared list functions

--- src/tracer/builtins/index.ts
+++ src/tracer/builtins/index.ts
@@ -1,13 +1,12 @@
-import { StepperBaseNode } from "../interface";
-import { StepperExpression } from "../nodes";
-import { StepperIdentifier } from "../nodes/Expression/Identifier";
-import { StepperLiteral } from "../nodes/Expression/Literal";
-import { listBuiltinFunctions } from "./lists";
-import { miscBuiltinFunctions } from "./misc";
-
+import { StepperBaseNode } from '../interface'
+import { StepperExpression } from '../nodes'
+import { StepperIdentifier } from '../nodes/Expression/Identifier'
+import { StepperLiteral } from '../nodes/Expression/Literal'
+import { listBuiltinFunctions } from './lists'
+import { miscBuiltinFunctions } from './misc'
 const builtinFunctions = {
-    ...listBuiltinFunctions,
-    ...miscBuiltinFunctions,
+  ...listBuiltinFunctions,
+  ...miscBuiltinFunctions
 }
 
 export function prelude(inputNode: StepperBaseNode) {
@@ -15,29 +14,27 @@ export function prelude(inputNode: StepperBaseNode) {
   Object.getOwnPropertyNames(Math)
     .filter(name => name in Math && typeof Math[name as keyof typeof Math] !== 'function')
     .forEach(name => {
-        inputNode = inputNode.substitute(
-            new StepperIdentifier('math_' + name), 
-            new StepperLiteral(Math[name as keyof typeof Math] as number)
-        )
-    });
-    return inputNode;
+      inputNode = inputNode.substitute(
+        new StepperIdentifier('math_' + name),
+        new StepperLiteral(Math[name as keyof typeof Math] as number)
+      )
+    })
+  return inputNode
 }
 
-
 export function getBuiltinFunction(name: string, args: StepperExpression[]): StepperExpression {
-    if (name.startsWith('math_')) {
-        const mathFnName = name.split('_')[1];
-        
-        if (mathFnName in Math) {
-            const fn = (Math as any)[mathFnName];
-            const argVal = args.map(arg => (arg as StepperLiteral).value);
-            return new StepperLiteral(fn(...argVal));
-        }
+  if (name.startsWith('math_')) {
+    const mathFnName = name.split('_')[1]
+
+    if (mathFnName in Math) {
+      const fn = (Math as any)[mathFnName]
+      const argVal = args.map(arg => (arg as StepperLiteral).value)
+      return new StepperLiteral(fn(...argVal))
     }
-    return builtinFunctions[name as keyof typeof builtinFunctions].definition(args);
+  }
+  return builtinFunctions[name as keyof typeof builtinFunctions].definition(args)
 }
 
 export function isBuiltinFunction(name: string): boolean {
-    return name.startsWith('math_') || Object.keys(builtinFunctions).includes(name);
+  return name.startsWith('math_') || Object.keys(builtinFunctions).includes(name)
 }
-
