dc271e9405a76cd6db50bc84c77c1708db98cbaa
Lee Yi <leeyi45@gmail.com>
2025-08-11 02:00:15 +0800

    Add vitest configuration

---
+++ vitest.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, coverageConfigDefaults, defaultExclude } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    name: 'js-slang',
+    environment: 'jsdom',
+    snapshotFormat: {
+      escapeString: true,
+      printBasicPrototype: true
+    },
+    include: ['src/**/__tests__/**/*.ts'],
+    exclude: [
+      ...defaultExclude,
+      "src/alt-langs/scheme/scm-slang",
+      ".*benchmark.*",
+      "**/__tests__/**/(.*/)?utils\\.ts"
+    ],
+    coverage: {
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "src/typings/",
+        "src/utils/testing",
+        "src/alt-langs/scheme/scm-slang",
+        "src/py-slang/"
+      ],
+      reporter: 'lcov'
+    },
+    reporters: ['html', 'default'],
+    // setupFiles: ['src/utils/testing/setup.ts']
+  }
+});
