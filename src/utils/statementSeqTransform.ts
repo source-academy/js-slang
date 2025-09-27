c709a62b89db001ae20bba5608c9a186114ed40b
Lee Yi <leeyi45@gmail.com>
2025-09-26 23:46:15 -0400

    More compact version of seq transform

--- src/utils/statementSeqTransform.ts
+++ src/utils/statementSeqTransform.ts
@@ -52,16 +52,8 @@ const transformers: NodeTransformers = {
     node.body = transform(node.body)
     return node
   },
-  AssignmentExpression: node => {
-    node.left = transform(node.left)
-    node.right = transform(node.right)
-    return node
-  },
-  BinaryExpression: node => {
-    node.left = transform(node.left)
-    node.right = transform(node.right)
-    return node
-  },
+  AssignmentExpression: ['left', 'right'],
+  BinaryExpression: ['left', 'right'],
   BlockStatement: node => {
     node.body = node.body.map(transform)
     if (hasDeclarations(node)) {
@@ -70,43 +62,16 @@ const transformers: NodeTransformers = {
       return ast.statementSequence(node.body, node.loc)
     }
   },
-  BreakStatement: node => {
-    if (node.label) {
-      node.label = transform(node.label)
-    }
-    return node
-  },
+  BreakStatement: 'label',
   CallExpression: node => {
     node.callee = transform(node.callee)
     node.arguments = node.arguments.map(transform)
     return node
   },
-  ClassDeclaration: node => {
-    if (node.id) {
-      node.id = transform(node.id)
-    }
-    if (node.superClass) {
-      node.superClass = transform(node.superClass)
-    }
-    node.body = transform(node.body)
-    return node
-  },
-  ConditionalExpression: node => {
-    node.test = transform(node.test)
-    node.consequent = transform(node.consequent)
-    node.alternate = transform(node.alternate)
-    return node
-  },
-  ContinueStatement: node => {
-    if (node.label) {
-      node.label = transform(node.label)
-    }
-    return node
-  },
-  ExportDefaultDeclaration: node => {
-    node.declaration = transform(node.declaration)
-    return node
-  },
+  ClassDeclaration: ['body', 'id', 'superClass'],
+  ConditionalExpression: ['alternate', 'consequent', 'test'],
+  ContinueStatement: 'label',
+  ExportDefaultDeclaration: 'declaration',
   ExportNamedDeclaration: node => {
     if (node.declaration) {
       node.declaration = transform(node.declaration)
@@ -117,30 +82,11 @@ const transformers: NodeTransformers = {
     }
     return node
   },
-  ExportSpecifier: node => {
-    node.local = transform(node.local)
-    node.exported = transform(node.exported)
-    return node
-  },
-  ExpressionStatement: node => {
-    node.expression = transform(node.expression)
-    return node
-  },
-  ForStatement: node => {
-    if (node.init) {
-      node.init = transform(node.init)
-    }
-    if (node.test) {
-      node.test = transform(node.test)
-    }
-    if (node.update) {
-      node.update = transform(node.update)
-    }
-    node.body = transform(node.body)
-    return node
-  },
+  ExportSpecifier: ['exported', 'local'],
+  ExpressionStatement: 'expression',
+  ForStatement: ['body', 'init', 'test', 'update'],
   FunctionDeclaration: node => {
-    node.params = node.params.map(x => transform(x))
+    node.params = node.params.map(transform)
     node.body = transform(node.body)
     if (node.id) {
       node.id = transform(node.id)
@@ -151,49 +97,21 @@ const transformers: NodeTransformers = {
     if (node.id) {
       node.id = transform(node.id)
     }
-    node.params = node.params.map(x => transform(x))
+    node.params = node.params.map(transform)
     node.body = transform(node.body)
     return node
   },
-  Identifier: node => node,
-  IfStatement: node => {
-    node.test = transform(node.test)
-    node.consequent = transform(node.consequent)
-    if (node.alternate) {
-      node.alternate = transform(node.alternate)
-    }
-    return node
-  },
+  IfStatement: ['alternate', 'consequent', 'test'],
   ImportDeclaration: node => {
     node.specifiers = node.specifiers.map(transform)
     node.source = transform(node.source)
     return node
   },
-  ImportDefaultSpecifier: node => {
-    node.local = transform(node.local)
-    return node
-  },
-  ImportSpecifier: node => {
-    node.local = transform(node.local)
-    node.imported = transform(node.imported)
-    return node
-  },
-  Literal: node => node,
-  LogicalExpression: node => {
-    node.left = transform(node.left)
-    node.right = transform(node.right)
-    return node
-  },
-  MemberExpression: node => {
-    node.object = transform(node.object)
-    node.property = transform(node.property)
-    return node
-  },
-  MethodDefinition: node => {
-    node.key = transform(node.key)
-    node.value = transform(node.value)
-    return node
-  },
+  ImportDefaultSpecifier: 'local',
+  ImportSpecifier: ['imported', 'local'],
+  LogicalExpression: ['left', 'right'],
+  MemberExpression: ['object', 'property'],
+  MethodDefinition: ['key', 'value'],
   NewExpression: node => {
     node.arguments = node.arguments.map(transform)
     return node
@@ -209,70 +127,41 @@ const transformers: NodeTransformers = {
       return ast.statementSequence(node.body as es.Statement[], node.loc)
     }
   },
-  Property: node => {
-    node.key = transform(node.key)
-    node.value = transform(node.value)
-    return node
-  },
-  RestElement: node => {
-    node.argument = transform(node.argument)
-    return node
-  },
-  ReturnStatement: node => {
-    if (node.argument) {
-      node.argument = transform(node.argument)
-    }
-    return node
-  },
-  SpreadElement: node => {
-    node.argument = transform(node.argument)
-    return node
-  },
+  Property: ['key', 'value'],
+  RestElement: 'argument',
+  ReturnStatement: 'argument',
+  SpreadElement: 'argument',
   StatementSequence: node => {
-    node.body = node.body.map(x => transform(x))
+    node.body = node.body.map(transform)
     return node
   },
-  Super: node => node,
-  ThisExpression: node => node,
-  ThrowStatement: node => {
-    node.argument = transform(node.argument)
-    return node
-  },
-  TryStatement: node => {
-    node.block = transform(node.block)
-    if (node.handler) {
-      node.handler = transform(node.handler)
-    }
-    if (node.finalizer) {
-      node.finalizer = transform(node.finalizer)
-    }
-    return node
-  },
-  UnaryExpression: node => {
-    node.argument = transform(node.argument)
-    return node
-  },
-  VariableDeclarator: node => {
-    node.id = transform(node.id)
-    if (node.init) {
-      node.init = transform(node.init)
-    }
-    return node
-  },
+  ThrowStatement: 'argument',
+  TryStatement: ['block', 'finalizer', 'handler'],
+  UnaryExpression: 'argument',
+  VariableDeclarator: ['id', 'init'],
   VariableDeclaration: node => {
     node.declarations = node.declarations.map(transform)
     return node
   },
-  WhileStatement: node => {
-    node.test = transform(node.test)
-    node.body = transform(node.body)
-    return node
-  }
+  WhileStatement: ['body', 'test']
 }
 
 export function transform<NodeType extends Node>(node: NodeType): NodeType {
-  if (!(node.type in transformers)) return node
+  const transformer = transformers[node.type]
 
-  const transformer = transformers[node.type] as NodeTransformer<NodeType>;
-  return transformer(node) as NodeType
+  switch (typeof transformer) {
+    case 'undefined':
+      return node
+    case 'function':
+      // @ts-expect-error Node type gets narrowed to never
+      return transformer(node) as NodeType
+  }
+
+  const properties = typeof transformer === 'string' ? [transformer] : transformer
+  for (const prop of properties) {
+    // @ts-expect-error Weird typescript shennenigans going on here don't mind this
+    node[prop!] = transform(node[prop])
+  }
+
+  return node
 }
