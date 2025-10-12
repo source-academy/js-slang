2de06d8f67389d62f6cfa0281103bbf0f6901e96
mug1wara26 <aloysiusgsq@gmail.com>
2025-10-13 00:25:56 +0800

    Check for const declaration in for statement init

---
+++ src/parser/source/rules/noConstDeclarationInForLoopInit.ts
@@ -0,0 +1,33 @@
+import type { ForStatement } from 'estree'
+import type { Rule } from '../../types'
+import { stripIndent } from '../../../utils/formatters'
+import { RuleError } from '../../errors'
+
+export class NoConstDeclarationInForLoopInit extends RuleError<ForStatement> {
+  constructor(node: ForStatement) {
+    super(node)
+  }
+  public explain(): string {
+    return 'Const declaration in init part of for statement is not allowed'
+  }
+  public elaborate(): string {
+    return stripIndent`
+      The init part of this statement cannot contain a const declaration, use a let declaration instead.
+    `
+  }
+}
+const noConstDeclarationInForLoopInit: Rule<ForStatement> = {
+  name: 'no-const-declaration-in-for-loop-init',
+
+  checkers: {
+    ForStatement(node) {
+      if (node.init && node.init.type === 'VariableDeclaration' && node.init.kind === 'const') {
+        return [new NoConstDeclarationInForLoopInit(node)]
+      }
+
+      return []
+    }
+  }
+}
+
+export { noConstDeclarationInForLoopInit }
