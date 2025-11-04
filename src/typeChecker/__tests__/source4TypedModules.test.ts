87a9c9400f010857893e7b88d46a976f9923b517
Toh Li Heng <tohliheng@gmail.com>
2025-11-05 00:50:44 +0800

    Language options (#1757)
    * Add language options, any checker and tests
    
    * Use Record instead of Map
    
    * Added tests for any checker
    
    * Added tests for type declaration
    
    * Fix incorrect merge conflict resolution
    
    * Fix incorrect test cases initialization
    
    * Fix compile errors post-merge
    
    * Fix line number shift
    
    ---------
    
    Co-authored-by: Richard Dominick <34370238+RichDom2185@users.noreply.github.com>

--- src/typeChecker/__tests__/source4TypedModules.test.ts
+++ src/typeChecker/__tests__/source4TypedModules.test.ts
@@ -1,7 +1,7 @@
-import { mockContext } from '../../utils/testing/mocks'
-import { Chapter, Variant } from '../../types'
-import { parse } from '../../parser/parser'
 import { parseError } from '../../index'
+import { parse } from '../../parser/parser'
+import { Chapter, Variant } from '../../types'
+import { mockContext } from '../../utils/testing/mocks'
 
 function getContext() {
   const context = mockContext(Chapter.SOURCE_4, Variant.TYPED)
@@ -14,6 +14,8 @@ function getContext() {
         class Test1 {}
         class Test2 {}
         class Test3 {}
+        type Test4 = (arg: Test1) => Test2;
+        const Test4 = (arg: Test1) => Test2;
       `,
       x: 'const x: string = "hello"',
       y: 'const y: number = 42',
@@ -179,7 +181,7 @@ describe('Typed module tests', () => {
         const a: string = functionError(10);
       `
       expect(testParseError(code)).toMatchInlineSnapshot(
-        `"Line 6: Type 'number' is not assignable to type 'string'."`
+        `"Line 8: Type 'number' is not assignable to type 'string'."`
       )
     })
 
@@ -260,5 +262,24 @@ describe('Typed module tests', () => {
         `"Line 3: Type 'Test3' is not assignable to type 'boolean'."`
       )
     })
+  })
+
+  /* TEST CASES FOR THE 'Test4' TYPE */
+  it('should allow calling Test4 with a valid Test1 object', () => {
+    const code = `
+      import { test2 } from 'exampleModule';
+      const result: Test4 = (arg: Test1) => test2;
+    `
+    expect(testParseError(code)).toMatchInlineSnapshot(`""`)
+  })
+
+  it('should error when calling Test4 with a string argument', () => {
+    const code = `
+      import { test1 } from 'exampleModule';
+      const result: Test4 = (arg: Test1) => test1;
+    `
+    expect(testParseError(code)).toMatchInlineSnapshot(
+      `"Line 3: Type '(Test1) => Test1' is not assignable to type 'Test4'."`
+    )
   })
 })
