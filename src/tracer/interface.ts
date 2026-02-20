180ab326f816623a98ca3de1a219d7cd76c3801d
DESKTOP-G08HS3B\Lee Yi <leeyi45@gmail.com>
2026-02-20 18:28:16 +0800

    Tidy up generator and explainer implementations for tracer

--- src/tracer/interface.ts
+++ src/tracer/interface.ts
@@ -3,9 +3,12 @@ import type { BaseNode, Comment, SourceLocation } from 'estree'
 import type { Node } from '../types'
 import type { StepperExpression, StepperPattern } from './nodes'
 
-export abstract class StepperBaseNode<TType extends Node = Node> implements BaseNode {
+/**
+ * Base type for all Stepper Nodes
+ */
+export abstract class StepperBaseNode<T extends Node = Node> implements BaseNode {
   constructor(
-    public readonly type: TType['type'],
+    public readonly type: T['type'],
     public readonly leadingComments?: Comment[] | undefined,
     public readonly trailingComments?: Comment[] | undefined,
     public readonly loc?: SourceLocation | null | undefined,
@@ -14,14 +17,17 @@ export abstract class StepperBaseNode<TType extends Node = Node> implements Base
 
   public abstract isContractible(): boolean
   public abstract isOneStepPossible(): boolean
-  public abstract contract(): StepperBaseNode<Node>
-  public abstract oneStep(): StepperBaseNode<Node>
-  public abstract substitute(id: StepperPattern, value: StepperExpression): StepperBaseNode<Node>
+  public abstract contract(): StepperBaseNode
+  public abstract oneStep(): StepperBaseNode
+  public abstract substitute(id: StepperPattern, value: StepperExpression): StepperBaseNode
   public abstract freeNames(): string[]
   public abstract allNames(): string[]
-  public abstract rename(before: string, after: string): StepperBaseNode<Node>
+  public abstract rename(before: string, after: string): StepperBaseNode
 
   public toReplString() {
+    // By providing this default toReplString implementation, when the nodes are stringified during
+    // error handling, we let astring handle the stringification of the nodes instead of js-slang's
+    // default internal implementation
     return generate(this)
   }
 }
