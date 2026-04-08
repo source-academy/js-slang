464c7ca21cb63a352f89ab1e311f1dc7951a68bc
TristanTayYuHng <33745288+TristanTayYuHng@users.noreply.github.com>
2026-04-08 15:57:16 +0800

    Yarn format

--- src/utils/testing/mocks.ts
+++ src/utils/testing/mocks.ts
@@ -61,7 +61,7 @@ export function mockRuntimeContext(): Context {
     envStepsTotal: 0,
     breakpointSteps: [],
     changepointSteps: [],
-    streamsPointSteps: []
+    streamsPointSteps: [],
   };
   return context;
 }
